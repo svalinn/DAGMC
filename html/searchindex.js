40a9c0564a6ac21e07634f116cf2226ffbc95707
Paul Wilson <wilsonp@engr.wisc.edu>
2012-08-31 10:10:31 -0500

    Adding Sphinx skeleton in a github appropriate way.

---
+++ html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,accler:0,direct:0,indic:0,toolkit:0,content:0,mont:0,tabl:0,geometri:0,document:0,carlo:0,modul:0,page:0},objtypes:{},titles:["Welcome to Direct Acclerated Geometry Monte Carlo Toolkit&#8217;s documentation!"],objnames:{},filenames:["index"]})
