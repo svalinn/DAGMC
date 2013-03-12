de91ccda3cbbb79ddb613ba8e98fb5856dcadece
Andrew Davis <davisa@davisa.ep.wisc.edu>
2013-03-12 08:38:13 -0500

    added functions to print edges and facets

--- functions.h
+++ functions.h
@@ -1,1 +1,3 @@
 void get_num_ents(MBInterface *mb); // return the number of entities of dimensions 0-3
+void get_line_data(MBInterface *mb); // return the number of entities of dimensions 0-3
+void get_tri_data(MBInterface *mb); // return the number of entities of dimensions 0-3
