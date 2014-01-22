1123c8b0083b7e0ce737522ed0c3d40745fcfcf8
Andrew Davis <davisa@engr.wisc.edu>
2014-01-21 20:25:16 -0600

    Added new default argument to ray_fire to allow entrance intersections to be allowed for geant4

--- tools/dagmc/DagMC.hpp
+++ tools/dagmc/DagMC.hpp
@@ -178,6 +178,7 @@ public:
                      const double ray_start[3], const double ray_dir[3],
                      EntityHandle& next_surf, double& next_surf_dist,
                      RayHistory* history = NULL, double dist_limit = 0,
+		     int ray_normal = 1, 
                      OrientedBoxTreeTool::TrvStats* stats = NULL );
   
   /**\brief Test if a point is inside or outside a volume 
