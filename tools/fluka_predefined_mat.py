563e1dcb199770f4e92fe63ec4c67115acdefe16
Moatazharb <mharb@wisc.edu>
2014-09-19 13:27:05 -0500

    list of predefined fluka materials

--- tools/fluka_predefined_mat.py
+++ tools/fluka_predefined_mat.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python
-"""List of pre-defined single-element FLUKA materials
-
- Fluka name   Fluka    Common name                    A         Z    Density
-              number                                                 [g/cm^3]
-
- BLCKHOLE       1       Blackhole or External Vacuum  0         0    0
- VACUUM         2       Vacuum or Internal Vacuum     0         0    0
- HYDROGEN       3       Hydrogen                      1.00794   1.   0.0000837
- HELIUM         4       Helium                        4.002602  2.   0.000166
- BERYLLIU       5       Beryllium                     9.012182  4.   1.848
- CARBON         6       Carbon                       12.0107    6.   2.000
- NITROGEN       7       Nitrogen                     14.0067    7.   0.00117
- OXYGEN         8       Oxygen                       15.9994    8.   0.00133
- MAGNESIU       9       Magnesium                    24.3050   12.   1.740
- ALUMINUM      10       Aluminium                    26.981538 13.   2.699
- IRON          11       Iron                         55.845    26.   7.874
- COPPER        12       Copper                       63.546    29.   8.960
- SILVER        13       Silver                      107.8682   47.  10.500
- SILICON       14       Silicon                      28.0855   14.   2.329
- GOLD          15       Gold                        196.96655  79.  19.320
- MERCURY       16       Mercury                     200.59     80.  13.546
- LEAD          17       Lead                        207.2      82.  11.350
- TANTALUM      18       Tantalum                    180.9479   73.  16.654
- SODIUM        19       Sodium                       22.989770 11.   0.971
- ARGON         20       Argon                        39.948    18.   0.00166
- CALCIUM       21       Calcium                      40.078    20.   1.550
- TIN           22       Tin                         118.710    50.   7.310
- TUNGSTEN      23       Tungsten                    183.84     74.  19.300
- TITANIUM      24       Titanium                     47.867    22.   4.540
- NICKEL        25       Nickel                       58.6934   28.   8.902
 """
+List of pre-defined single-element FLUKA materials
+"""
+Fluka_predefined_mat=["BERYLLIU","BARIUM","BISMUTH","BROMINE","RHENIUM","RUTHERFO","ROENTGEN","HYDROGEN","PHOSPHO","GERMANIU",
+"GADOLINI","GALLIUM","HASSIUM","ZINC","HAFNIUM","MERCURY","HELIUM","PRASEODY","PLATINUM","239-PU","LEAD","CARBON", "POTASSIU","OXYGEN","SULFUR","TUNGSTEN","EUROPIUM","MAGNESIU","MOLYBDEN","MANGANES","URANIUM","IRON","NICKEL","NITROGEN",
+"SODIUM","NIOBIUM","NEODYMIU","NEON","ZIRCONIU","BORON","COBALT","FLUORINE","CALCIUM","CERIUM","CADMIUM","VANADIUM","CESIUM",
+"CHROMIUM","COPPER","STRONTIU","KRYPTON","SILICON","TIN","SAMARIUM","SCANDIUM","ANTIMONY","LANTHANU","CHLORINE","LITHIUM",
+"TITANIUM","TERBIUM","99-TC", "TANTALUM","SILVER","IODINE","IRIDIUM","241-AM","ALUMINUM","ARSENIC","ARGON","GOLD","INDIUM",
+"YTTRIUM","XENON","HYDROG-1","DEUTERIU","TRITIUM","HELIUM-3","HELIUM-4","LITHIU-6","LITHIU-7","BORON-10","BORON-11","90-SR",
+"129-I","124-XE","126-XE","128-XE","130-XE","131-XE","132-XE","134-XE","135-XE","136-XE","135-CS","137-CS", "230-TH","232-TH",
+"233-U","234-U","235-U","238-U"]
 
 
 """
 def: Fluka_list is a list of predefined Fluka materials (only names)
 """
-
+"""
 Fluka_predefined_mat=['BLCKHOLE', 'VACUUM', 'HYDROGEN', 'HELIUM', 'BERYLLIU', 'CARBON', 'NITROGEN', 'OXYGEN', 'MAGNESIU', 'ALUMINUM', 'IRON', 'COPPER', 'SILVER', 'SILICON', 'GOLD', 'MERCURY', 'LEAD', 'TANTALUM', 'SODIUM', 'ARGON', 'CALCIUM', 'TIN', 'TUNGSTEN', 'TITANIUM', 'NICKEL']
 
-
+"""
