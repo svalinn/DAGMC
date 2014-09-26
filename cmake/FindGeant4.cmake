14f7514d5073b799490b58d8d2b5cce6623ac41b
Andrew Davis <davisa@engr.wisc.edu>
2014-09-25 22:20:59 -0500

    added findgeant4.cmake

---
+++ cmake/FindGeant4.cmake
@@ -0,0 +1,25 @@
+# Try to find MOAB
+#
+# Once done this will define
+#
+#  MOAB_FOUND - system has MOAB
+#  MOAB_INCLUDE_DIRS - the MOAB include directory
+#  MOAB_LIBRARIES - Link these to use MOAB
+#  MOAB_DEFINITIONS - Compiler switches required for using MOAB
+
+# GEANT4_FOUND - System has Geant4
+# GEANT4_INCLUDE_DIR - The Geant4 include dir
+# GEANT4_LIBRARIES - In case they are ever needed
+
+find_path( GEANT4_CMAKE_CONFIG NAMES Geant4Config.cmake
+  HINTS ${GEANT4_DIR}/bld
+  PATHS ENV LD_LIBRARY_PATH
+  PATH_SUFFIXES lib Lib
+  NO_DEFAULT_PATH
+  )
+
+MESSAGE ( STATUS "Found Geant4 in ${GEANT4_CMAKE_CONFIG}" )
+
+INCLUDE ( ${GEANT4_CMAKE_CONFIG}/Geant4Config.cmake )
+
+
