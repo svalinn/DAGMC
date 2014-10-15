b041dcf33c7463f8844844bcc6bb3e547050633e
pshriwise <shriwise@wisc.edu>
2014-10-15 15:49:10 -0500

    Added comments and re-ordered coefficients for clarity.

--- tools/dagmc/ray_fire_test.cc
+++ tools/dagmc/ray_fire_test.cc
@@ -43,9 +43,9 @@ static const double denomPI = PI * denom;
   
 inline void RNDVEC(CartVect& uvw, double &az) 
 {
-  
-  double theta = denom * az * rand();
-  double u = 2*denom *rand() - 1;
+  // denom normalizes rand values (see global defines)
+  double theta = az * denom * rand(); // randomly samples from 0 to az. (Default az is 2PI)
+  double u = 2 * denom * rand() - 1; // randomly samples from -1 to 1.
   uvw[0] = sqrt(1-u*u)*cos(theta);
   uvw[1] = sqrt(1-u*u)*sin(theta);
   uvw[2] = u;
