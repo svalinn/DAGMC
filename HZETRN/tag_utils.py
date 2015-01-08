007e3c63bd9b955377977a5bceb3f8e207103467
Julie C. Zachman <zachman@engr.wisc.edu>
2015-01-08 14:56:18 -0600

    Rework tag_utils per hackathon review

--- HZETRN/tag_utils.py
+++ HZETRN/tag_utils.py
@@ -20,7 +20,7 @@ tag : string of the tag to add to tag_list
 tag_list : vector of tags in the problem
 returns tag_list
 """
-def tag_to_script(tag):
+def _stringify(tag):
     a = []
     # since we have a byte type tag loop over the 32 elements
     for part in tag:
@@ -29,11 +29,11 @@ def tag_to_script(tag):
             # convert to ascii
             a.append(str(unichr(part)))
             # join to end string
-            test = ''.join(a)
+            string = ''.join(a)
             # the the string we are testing for is not in the list of found
             # tag values, add to the list of tag_values
 
-    return test
+    return string
 
 """
 function that gets all the material tags on dagmc geometry
@@ -49,56 +49,32 @@ def get_mat_tag_values(path):
     dag_geom = iMesh.Mesh()
     dag_geom.load(path)
 
-    # get all entities      
-    ents = dag_geom.getEntities()
     # create a  mesh set         
-    mesh_set = dag_geom.getEntSets()
-    # list of volume ent handles and geom_dimension ents
-    mat_list = []
-    geom_list = []
+    mesh_sets = dag_geom.getEntSets()
 
-    vol_tag  = dag_geom.getTagHandle('GEOM_DIMENSION')
-    name_tag = dag_geom.getTagHandle('GLOBAL_ID')
-    mat_tag  = dag_geom.getTagHandle('NAME')
+    cat_tag  = dag_geom.getTagHandle('CATEGORY')
+    id_tag   = dag_geom.getTagHandle('GLOBAL_ID')
+    name_tag = dag_geom.getTagHandle('NAME')
 
     # get the list we need    
-    for i in mesh_set:
-        tags = dag_geom.getAllTags(i)
-        for tag in tags:
-            if tag == vol_tag:
-                geom_list.append(i)
-            if tag == mat_tag:
-                mat_list.append(i)
+    for mesh_set in mesh_sets:
+        tags = dag_geom.getAllTags(mesh_set)
+	if name_tag in tags and cat_tag in tags and _stringify(cat_tag[mesh_set])=="Group":
+	    name = _stringify(name_tag[mesh_set])
+	    # Now get the volumes of this group
+	    child_sets = mesh_set.getEntSets()
+	    for child in child_sets:
+	        vol_id = id_tag[child]
+	        if 'mat:' in name:
+	            vol_mat_dict[vol_id] = name
 
+	# else this is a meshset that doesn't have volume
 
-    found_meshset_for_vol = False
-    # loop over all the volumes 
-    for entity in geom_list:
-        # loop over the material sets
-        for meshset in mat_list:
-	    # reset
-            found_meshset_for_vol = False
-            # if volume in set       
-            if meshset.contains(entity):
-	        if found_meshset_for_vol:
-		    # ToDo: throw exception:, should not be here again for same vol
-	            print 'Error: a volume cannot belong to more than one material'
-		# Expect a volume to be in only one mat_list set
-		found_meshset_for_vol = True
-                mat_name = mat_tag[meshset]
-		# print ('in entity-meshset loop: ', mat_tag, meshset, mat_tag[meshset])
-                volume_name = name_tag[entity]
-
-		# uwuw_preproc's version, get_tag_values(), uses 'tally:' here
-		if 'mat:' in tag_to_script(mat_name):
-		    vol_mat_dict[volume_name] = tag_to_script(mat_name)
-		  
     return vol_mat_dict
 
 def get_fnames_for_vol(path):
     vol_fname_dict = {}
     vol_mat_dict = get_mat_tag_values(path)
-    print 'materials and volumes', vol_mat_dict
     # Cross-Section: load the material library from the uwuw geometry file
     mat_lib = material.MaterialLibrary()
     mat_lib.from_hdf5(path)
