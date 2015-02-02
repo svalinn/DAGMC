8ccab537a36f43f44d65cc32e5a770d2b876cc1e
Julie C. Zachman <zachman@engr.wisc.edu>
2015-02-02 09:27:15 -0600

    Correctly processes depth-based data using names.txt

--- HZETRN/results.py
+++ HZETRN/results.py
@@ -6,9 +6,11 @@ import glob
 import numpy as np
 import csv
 
-dose_filebase  = 'ascii_dose_table'
-
-doseq_filebase = 'ascii_doseq_table'
+# Global names to be read from a separate text file
+dose_filename  = ''
+doseq_filename = ''
+dose_ple_filename  = ''
+doseq_ple_filename = ''
 
 class Results:
 
@@ -17,17 +19,27 @@ class Results:
     def __init__(self, dir):
         self.directory = dir
 
-    def process(self, name, n):
+    def last_lines(self, name, n):
 	filepath = self.directory + name
         return check_last_n_lines(filepath, n)
 
 class nx2(Results):
 
-    def process(self, name, n):
-        result_line = Results.process(self, name, n)
-	cols = result_line[0].split()
-	print cols
-        return cols
+    def last_lines(self, name):
+        result_line = Results.last_lines(self, name, 1)
+	words = result_line[0].split()
+	# We are skipping the first value, which is the depth
+        vals = np.array(float(words[1]))
+	return vals
+
+class nx7(Results):
+
+    def last_lines(self, name):
+        result_line = Results.last_lines(self, name, 1)
+	words = result_line[0].split()
+	# We are skipping the first value, which is the depth
+        vals = np.array([float(words[1]), float(words[2]), float(words[3]), float(words[4]), float(words[5]), float(words[6])])
+	return vals
 
 
 def reversed_lines(file):
@@ -67,6 +79,42 @@ def check_last_n_lines(file, n):
     # the last n lines in the same order as they are in the file
     return lines[::-1]
 
+def get_names(file):
+    "Fill in the global filenames from a text file."
+    global dose_filename, doseq_filename, dose_ple_filename, doseq_ple_filename
+    lines = []
+    with open(file,'r') as f:
+	# Get read of the newline
+        for line in f:
+            lines.append(line.split('\n')[0])
+    dose_filename      = lines[0]
+    doseq_filename     = lines[1]
+    dose_ple_filename  = lines[2]
+    doseq_ple_filename = lines[3]
+    return 
+
+def depth_header():
+    "Return a header for the depth data"
+    header =  '\n' + \
+              '{0: >12}'.format('X') + \
+              '{0: >15}'.format('Y') + \
+              '{0: >15}'.format('Z') + \
+              '{0: >15}'.format('Dose_All') + \
+              '{0: >15}'.format('Doseq_All') + \
+              '{0: >15}'.format('dose-neutron') + \
+              '{0: >15}'.format('dose-proton') + \
+              '{0: >15}'.format('dose-deut.') + \
+              '{0: >15}'.format('dose-trit.') + \
+              '{0: >15}'.format('dose-H3') + \
+              '{0: >15}'.format('dose-H4') + \
+              '{0: >15}'.format('doseq-neutron') + \
+              '{0: >15}'.format('doseq-proton') + \
+              '{0: >15}'.format('doseq-deut.') + \
+              '{0: >15}'.format('doseq-trit.') + \
+              '{0: >15}'.format('doseq-H3') + \
+              '{0: >15}'.format('doseq-H4') + '\n'
+    return header
+
 """
 Argument parsing
 returns : args: -d for the run directory
@@ -85,55 +133,73 @@ def parsing():
     return args
 
 def main():
+    global dose_filename, doseq_filename, dose_ple_filename, doseq_ple_filename
 
     # Setup: parse the the command line parameters
     args = parsing()
-    curdir = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+    # Set up the names of the data files without them being in this file
+    get_names("names.txt")
+    # print dose_filename, doseq_filename, dose_ple_filename, doseq_ple_filename
+    
+    # Original Working Directory - will be used for the results file
+    owd = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+    # Directory containing the ray subdirectories
     run_path = args.run_dir + '/data/'
 
     os.chdir(run_path)
-    g = nx2('')
+    depth_data_reader = nx2('')
+    depth_particle_reader = nx7('')
 
+    all_depth = np.array([])
+    # This could be up to 10,000 subdirectories
     ray_subdirs = glob.glob('*')
-    doses = []
-    doseqs = []
-    header = ",X Y Z Dose-Depth Dose Doseq-Depth Doseq"
-    # Open as write, then append, in order to clear previous
-    with open('../results_data.csv', 'w') as s:
-	s.write(header + '\n')
-        s.close
-    with open('../results_data.csv', 'a') as s:
-        for r in ray_subdirs:
+    for r in ray_subdirs:
+	dose_filepath  = r + '/' + dose_filename 
+	doseq_filepath = r + '/' + doseq_filename
+	dose_part_filepath  = r + '/' + dose_ple_filename 
+	doseq_part_filepath = r + '/' + doseq_ple_filename
+	    
+	if os.path.exists(dose_filepath) and os.path.exists(doseq_filepath) and \
+	   os.path.exists(dose_part_filepath) and os.path.exists(doseq_part_filepath):
             ray = r.split('_') 
-	    dosefile  = r + '/' + dose_filebase + '.dat'
-	    doseqfile = r + '/' + doseq_filebase + '.dat'
-	    # print 'ray = ', ray
-	    if os.path.exists(dosefile) and os.path.exists(doseqfile):
-		line = "," + ray[0] + "," +  ray[1] + "," + ray[2] + ","
-	        cols = g.process(dosefile, 1)
-		line += cols[0] + "," + cols[1] + ","
-	        doses.append(float(cols[len(cols)-1]))
-	        # print ray, cols
-
-	        cols = g.process(doseqfile,1)
-	        doseqs.append(float(cols[len(cols)-1]))
-		line += cols[0] + "," + cols[1] + "\n"
-		s.write(line)
-
-        print 'doses', doses
-	dose_ave = np.average(doses)
-	dose_std = np.std(doses)
-        print 'Average dose', "{:1.6E}".format(dose_ave)
-        print 'Std. Dev. dose', "{:1.6E}".format(dose_std)
+            # Add ray data to line as separate floates
+	    ray_vec = np.array([float(ray[0]), float(ray[1]), float(ray[2])])
+            # Add dose and doseq at depth for 1 or 6 particles
+	    hnums = depth_data_reader.last_lines(dose_filepath)
+	    inums = depth_data_reader.last_lines(doseq_filepath)
+	    jnums = depth_particle_reader.last_lines(dose_part_filepath)
+	    knums = depth_particle_reader.last_lines(doseq_part_filepath)
+	   
+	    one_line = np.hstack((ray_vec,hnums,inums,jnums,knums))
+	    # First time through...
+	    if all_depth.size == 0:
+	        all_depth = one_line
+	    # ...otherwise append ('vertically stack')
+	    else:
+	        all_depth = np.vstack((all_depth,one_line)) 
     
+    ave_at_depth = np.average(all_depth,0)
+    std_at_depth = np.std(all_depth,0)
 
-        print 'doseqs', doseqs
-	doseq_ave = np.average(doseqs)
-	doseq_std = np.std(doseqs)
-        ave_doseq_str =  "{:1.6E}".format(doseq_ave)
-        std_doseq_str = "{:1.6E}".format(doseq_std)
-	s.write("Averages,,,,," +  "{:1.6E}".format(dose_ave) + ",," +  ave_doseq_str + "\n") 
-	#s.write('Std Dev', '', '', '', dose_std, '', doseq_std) 
+    dashes15  = '{0: >15}'.format('--') + '{0: >15}'.format('--')
+    ave_words = '{0: >12}'.format('--') + dashes15
+    std_words = '{0: >12}'.format('--') + dashes15
+
+    for i in range(3,17):
+        ave_words += "{:15.6E}".format(ave_at_depth[i])
+        std_words += "{:15.6E}".format(std_at_depth[i])
+
+    depth_hdr = depth_header()
+    footer = depth_hdr + '\n' + \
+            '{0: <12}'.format('Averages')  + '\n' + ave_words + '\n' + \
+	    '{0: <12}'.format('Std.-Dev.') + '\n' + std_words + '\n' 
+
+    os.chdir(owd)
+    # ToDo:  This appends to a previously existing file of the same name; it should be cleared first
+    f_handle = file('tmp1.dat', 'a')
+    np.savetxt(f_handle, all_depth, '%14.6E', delimiter=' ', newline='\n', header=depth_hdr, footer=footer) 
 
 
 if __name__ == '__main__':
