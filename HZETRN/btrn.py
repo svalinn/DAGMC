0b52c694bf86fc4f87a272b10b5fec69aa8802cf
Julie C. Zachman <zachman@engr.wisc.edu>
2015-05-20 22:11:48 -0500

    Call hzetrn method on class; improve slab algorithm

--- HZETRN/btrn.py
+++ HZETRN/btrn.py
@@ -20,8 +20,7 @@ except:
     raise ImportError("The DAGMC dependency could not be imported.")    
 
 import numpy as np
-# import hzetrn as one_d_tool
-from hzetrn import one_d_tool
+from hzetrn import transportresponse
 import tag_utils
 
 
@@ -32,7 +31,7 @@ def load_ray_tuples(filename):
     ----------
     filename : string
         filename of file containing pairs of angles (radians) representing polar and
-	azimuthal angles. 
+    azimuthal angles. 
 
     Returns
     -------
@@ -49,14 +48,15 @@ def load_ray_tuples(filename):
     
     with open(filename) as f:
         for i, line in enumerate(f):
-	    nums = map(float, line.split())
-	    if len(nums) < 2:
-	        msg = "Ray direction file {} requires two floats on each line; line {} is ill-formed".format(filename, i)
-		sys.exit(msg)
-	    w = np.cos(nums[0])
-	    u = np.sin(nums[0])*np.cos(nums[1])
-	    v = np.sin(nums[0])*np.sin(nums[1])
-	    ray_tuples.append((u, v, w))
+            nums = map(float, line.split())
+            if len(nums) < 2:
+                msg = "Ray direction file {} requires two floats on each line; \
+                       line {} is ill-formed".format(filename, i)
+                sys.exit(msg)
+            w = np.cos(nums[0])
+            u = np.sin(nums[0])*np.cos(nums[1])
+            v = np.sin(nums[0])*np.sin(nums[1])
+            ray_tuples.append((u, v, w))
     return ray_tuples
 
 def load_directions(ray_dir_file, num_ray_dirs):
@@ -66,17 +66,17 @@ def load_directions(ray_dir_file, num_ray_dirs):
     ----------
     ray_dir_file : string, filename 
         filename of file containing pairs of angles (radians) representing polar and
-	azimuthal angles.  Does not have to be set.
+    azimuthal angles.  Does not have to be set.
 
     num_ray_dirs : integer
         Number of ray directions to use.  
-	o If a filename is given and num_ray_dirs is
-	  nonzero and less than the number of directions in the file, the return array 
-	  is truncated to be of length num_ray_dirs.
-	o If no filename is given and num_ray_dirs > 0, num_ray_dirs random
-	  directions are generated
-	o If no filename is given and num_ray_dirs = 0, three directions are
-	  generated to be the Cartesian axes.
+    o If a filename is given and num_ray_dirs is
+      nonzero and less than the number of directions in the file, the return array 
+      is truncated to be of length num_ray_dirs.
+    o If no filename is given and num_ray_dirs > 0, num_ray_dirs random
+      directions are generated
+    o If no filename is given and num_ray_dirs = 0, three directions are
+      generated to be the Cartesian axes.
         
 
     Returns
@@ -85,17 +85,16 @@ def load_directions(ray_dir_file, num_ray_dirs):
     """
     # Return the ray directions: use a cutoff if provided
     if ray_dir_file:
+        print 'num_ray_dirs', num_ray_dirs
         ray_tuples = load_ray_tuples(ray_dir_file)
-	if num_ray_dirs > 0:
-	    ray_tuples = ray_tuples[:num_ray_dirs]
-
+        if num_ray_dirs > 0:
+            ray_tuples = ray_tuples[:num_ray_dirs]
     elif num_ray_dirs > 0:
         ray_tuples = tag_utils.get_rand_dirs(num_ray_dirs)
- 
     else:
         ray_tuples = [(1.0, 0.0, 0.0),
                       (0.0, 1.0, 0.0),
-	   	      (0.0, 0.0, 1.0)]
+                      (0.0, 0.0, 1.0)]
     return np.array(ray_tuples)
 
 
@@ -115,8 +114,8 @@ def find_special_vols(ref_point, material_name_dict):
 
     material_name_dict : dictionary, { string : int }
         A mapping of vol id to material object
-	Key   = vol_id : int
-	Value = unique material name
+    Key   = vol_id : int
+    Value = unique material name
 
     Returns
     -------
@@ -125,11 +124,11 @@ def find_special_vols(ref_point, material_name_dict):
 
     graveyard_vol : int
         The vol_id of the volume whose unique name is 'graveyard'.
-	
+    
     Exceptions
     ----------
         Raises exception if no volume is named 'graveyard'
-	Raises exception if none of the volumes contain the reference point
+    Raises exception if none of the volumes contain the reference point
 
     Notes
     -----
@@ -143,16 +142,17 @@ def find_special_vols(ref_point, material_name_dict):
     volumes = dagmc.get_volume_list() 
     for v in volumes:
         if dagmc.point_in_volume(v, ref_point):
-	    ref_vol = v
-	# Must check for inclusion in case it's the implicit complement, which
-	# is not in the dict
-	elif v in material_name_dict and material_name_dict[v] == 'graveyard':
-	    graveyard_vol = v
+            ref_vol = v
+        # Must check for inclusion in case it's the implicit complement, which
+        # is not in the dict
+        elif v in material_name_dict and material_name_dict[v] == 'graveyard':
+            graveyard_vol = v
 
     if ref_vol == -1:
         raise Exception("Reference volume not found!")
     if graveyard_vol == -1:
-        raise Exception("Graveyard vol not found!  Please use a geometry file tagged with a graveyard.")
+        raise Exception("Graveyard vol not found!  Please use a geometry file \
+                         tagged with a graveyard.")
 
     return ref_vol, graveyard_vol
 
@@ -180,8 +180,8 @@ def find_slabs_for_ray(graveyard_vol, start_vol, ref_point, uvw, material_name_d
 
     material_name_dict : dictionary, { string : int }
         A mapping of vol id to material object
-	Key   = vol_id : int
-	Value = unique material name
+    Key   = vol_id : int
+    Value = unique material name
 
     Returns
     -------
@@ -190,7 +190,7 @@ def find_slabs_for_ray(graveyard_vol, start_vol, ref_point, uvw, material_name_d
     
     slab_mat_names : list of strings
         Unique names of slabs in the same order as they are encountered,
-	meaning in the same order as the slab_lengths
+    meaning in the same order as the slab_lengths
 
     Notes
     -----
@@ -207,43 +207,123 @@ def find_slabs_for_ray(graveyard_vol, start_vol, ref_point, uvw, material_name_d
 
     last_vol = start_vol
     for (next_vol, dist, surf, xyz) in dagmc.ray_iterator(last_vol, ref_point, uvw, yield_xyz=True):
-
         if last_vol in material_name_dict:
             slab_lengths.append(dist)
-   	    # Get the unique name associated with volume
-	    slab_mat_names.append(material_name_dict[last_vol])
+            # Get the unique name associated with volume
+            slab_mat_names.append(material_name_dict[last_vol])
 
-	if next_vol == graveyard_vol or dist >= huge or surf == 0:
-	    break;
+        if next_vol == graveyard_vol or dist >= huge or surf == 0:
+            break;
 
         # If last_vol is the implicit complement, this is the only line executed
-        last_vol = next_vol	    
-
-	# Previous method: leave in for comparison for now
-	"""
-        is_graveyard = False    
-        if not is_graveyard and ((dist < huge) and (surf != 0)):
-	   
-	    # We have already checked last_vol for being a graveyard
-	    if last_vol in fname_for_vol:
-	        slab_lengths.append(dist)
-		# Get a name associated with volume
-	        slab_mat_names.append(fname_for_vol[last_vol])
-
-	    if next_vol in fname_for_vol:
-   	        if fname_for_vol[next_vol] == 'graveyard':
-	            is_graveyard = True
-
-            # If last_vol is the implicit complement, this is the only line executed
-            last_vol = next_vol
-
-	# We hit the graveyard
-        else:
-	    break
-	"""
+        last_vol = next_vol        
 
     return slab_lengths, slab_mat_names
       
+def spatialTuples(slab_mat_names, slab_lens):
+    """ Implement an algorithm to create tuples from the material/distances
+    lists that are in a convenient form for creating transport execution.
+
+    Parameters
+    ----------
+    slab_mat_names : list of strings
+        Unique material names from the geometry, in the order they are 
+    encountered for a particular ray.
+    
+    slab_lens : list of floats
+        Distances traveled through each material by the ray, in the same
+    order as slab_mat_names
+
+    Return
+    ------
+    spatial : list of tuples
+        Tuples are (string, list of floats)
+    First element of list of floats is always zero
+    List of floats is length 2 at a minimum
+    """
+    spatial = []
+    last_mat_name = slab_mat_names[0]
+    current_slab_pts = [0.0]
+    for mat_name, slab_thick in zip(slab_mat_names, slab_lens):
+        if mat_name != last_mat_name:
+            # Every time we get a new mat, store the name/slab points tuple...
+            spatial.append((last_mat_name, current_slab_pts))
+            # ... and re-initialize
+            last_mat_name = mat_name
+            current_slab_pts = [0.0]
+        # Do always; first time through, this is all that is done
+        current_slab_pts.append(current_slab_pts[-1] + slab_thick)
+
+    # Once more for the trailing tuple 
+    spatial.append((last_mat_name, current_slab_pts))
+
+    return spatial
+
+def createTransportDictionary(uvw, graveyard_vol, start_vol, ref_point, material_name_dict):
+    """ Create a dictionary containing material names and distances for use in 
+    one-dimensional transport code.
+
+    Parameters
+    ----------
+    uvw : list of 3 floats
+        The direction vector of a unit vector.  The final ray going through 
+    the material points in this direction.
+
+    graveyard_vol : int
+        Vol id of the graveyard, a convenience value
+
+    start_vol : int
+        Vol id of the volume containing the reference point.
+
+    ref_point : list of 3 floats
+        Triplet with the location of the point at which data is desired
+
+    material_name_dict : dictionary
+        Mapping from each volume in the geometry to a unique "fluka" name 
+        created by uwuw_preproc
+       
+    Return
+    ------
+    spatial : list of tuples
+        Tuples are (string, list of floats)
+    First element of list of floats is always zero
+    List of floats is length 2 at a minimum
+
+    Float value of the distance from the start of the overall ray to the 
+        reference point
+
+    Note
+    ----
+    This is the first place we can know if the rays did not go through a 
+    material.
+    """
+
+    # Rays fired here!
+    ###################################
+    # Create two lists 'a' and 'b'.  
+    #    'a' is the list of slab materials and lengths in the given direction.  
+    #    'b' is the list of same items going OPPOSITE (180 degrees)
+    slab_lens_a, slab_mat_names_a = find_slabs_for_ray(graveyard_vol, \
+                                                       start_vol,     \
+                               ref_point,     \
+                               uvw,           \
+                               material_name_dict)
+
+    slab_lens_b, slab_mat_names_b = find_slabs_for_ray(graveyard_vol, \
+                                                       start_vol,     \
+                               ref_point,     \
+                               -uvw,          \
+                                       material_name_dict)
+
+    # The ray did not go through any material
+    if len(slab_lens_b) + len(slab_lens_a) == 0:
+        return [], -1
+
+    slab_lens      = slab_lens_b[::-1]      + slab_lens_a
+    slab_mat_names = slab_mat_names_b[::-1] + slab_mat_names_a
+
+    return spatialTuples(slab_mat_names, slab_lens), len(slab_lens_b)
+
 def parse_command_line_arguments():
     """Perform command line argument parsing
     Notes
@@ -262,39 +342,39 @@ def parse_command_line_arguments():
 
     parser.add_argument(
         '-f', action='store', dest='uwuw_file', 
-	help='The relative path to the .h5m geometry file')
+    help='The relative path to the .h5m geometry file')
 
     parser.add_argument(
         '-d', '--rundir', action='store', 
-	help='The name of the holding directory for all hzetrn runs')
+    help='The name of the holding directory for all hzetrn runs')
     parser.set_defaults(rundir='rundir')
 
     parser.add_argument(
         '-e', '--environment',
-	help='The radiation environment: spe (default) or gcr')
+    help='The radiation environment: spe (default) or gcr')
     parser.set_defaults(environment='spe')
 
     parser.add_argument(
         '-r', action='store', dest='ray_dir_file', 
-	help='The path to the file with ray direction tuples')
+    help='The path to the file with ray direction tuples')
     parser.set_defaults(ray_dir_file='')
 
     parser.add_argument(
         '-n', action='store', dest='num_ray_dirs', 
-	help='The number of ray directions to use from file, \
-	      or randomly create if no file given.  \
-	      If neither file nor n is given, use Cartesion axes.', type=int)
+    help='The number of ray directions to use from file, \
+          or randomly create if no file given.  \
+          If neither file nor n is given, use Cartesion axes.', type=int)
     parser.set_defaults(num_ray_dirs=0)
 
     parser.add_argument(
         '-p', '--ref_point', nargs=3, type=float,
-	help='Cartesion coordinates of the reference point for measurements')
+    help='Cartesion coordinates of the reference point for measurements')
     parser.set_defaults(ref_point=[0.0,0.0,0.0])
 
     parser.add_argument(
         '-s', '--response', action='store', 
         help='The material in which to calculate the response.  \
-	      NB: the response material cross-section must exist')
+          NB: the response material cross-section must exist')
     parser.set_defaults(response='WATERLIQ')
 
     args = parser.parse_args()
@@ -305,105 +385,6 @@ def parse_command_line_arguments():
         raise Exception('h5m file path not specified. [-f] not set')
     
     return args
-
-"""
-def generateTransportInfo(slab_lens, slab_mat_names):
-        Extract and refine information from two lists.
-
-	Parameters
-	----------
-        slab_lens : list of floats
-	    Distances traveled through all non-graveyard, 
-	    non-vacuum slabs in the geometry
-        
-	slab_mat_names : list of strings
-	    Ordered list names of all non-vacuum, non-graveyard
-	    fluka (unique) names from the tagged geometry.  
-	    Matches the slab_lens list.
-
-	Requirements
-	------------
-	slab_lens is not empty
-
-        num_slabs = len(slab_lens)
-	# Initialize list
-	last_mat_name    = slab_mat_names[0]
-	last_slab_points = [0.0, slab_lens[0]]
-
-	for n in range(1,num_slabs):
-            if slab_mat_names[n] == last_mat_name:
-                last_slab_points.append(slab_lens[n] + slab_lens[n-1])
-	    else:
-
-	        #tmp_mat_name    = last_mat_name
-		#tmp_slab_points = last_slab_points
-		# trn_dict[last_mat_name] = last_slab_points
-	        yield last_mat_name, last_slab_points
-                # update internal state before leaving
-		last_mat_name = slab_mat_names[n]
-		last_slab_points = [0.0, slab_lens[n]]
-	        # yield trn_dict
-		#yield tmp_mat_name, tmp_slab_points
-	 
-	    yield last_mat_name, last_slab_points
-	    # trn_dict[last_mat_name] = last_slab_points
-	    # yield trn_dict
-"""
-
-def createTransportDictionary(uvw, graveyard_vol, start_vol, ref_point, material_name_dict):
-    """ Create a dictionary containing material names and distances for use in 
-        one-dimensional transport code.
-
-	Note
-	----
-	This is the first place we can know if the rays did not go through a 
-	material.
-    """
-
-    # Rays fired here!
-    ###################################
-    # Create two lists 'a' and 'b'.  
-    #    'a' is the list of slab materials and lengths in the given direction.  
-    #    'b' is the list of same items going OPPOSITE (180 degrees)
-    slab_lens_a, slab_mat_names_a = find_slabs_for_ray(graveyard_vol, \
-                                                       start_vol,     \
-						       ref_point,     \
-						       uvw,           \
-						       material_name_dict)
-
-    slab_lens_b, slab_mat_names_b = find_slabs_for_ray(graveyard_vol, \
-                                                       start_vol,     \
-						       ref_point,     \
-						       -uvw,          \
-				                       material_name_dict)
-
-    spatial_dict = {}
-    # If the ray does not go through any materials the yield is ({}, -1)
-    if len(slab_lens_b) + len(slab_lens_a) == 0:
-        return spatial_dict, -1
-
-    slab_lens      = slab_lens_b[::-1]      + slab_lens_a
-    slab_mat_names = slab_mat_names_b[::-1] + slab_mat_names_a
-
-    # Initialize the id-dictionary
-    num_slabs = len(slab_lens)
-    last_mat_name    = slab_mat_names[0]
-    last_slab_points = [0.0, slab_lens[0]]
-
-    for n in range(1,num_slabs):
-        if slab_mat_names[n] == last_mat_name:
-	    trn_dict = {}
-            trn_dict[last_mat_name] = last_slab_points
-	    spatial_dict[n-1] = trn_dict
-            # update internal state before leaving
-	    last_mat_name = slab_mat_names[n]
-	    last_slab_points = [0.0, slab_lens[n]]
-	 
-    trn_dict = {}
-    trn_dict[last_mat_name] = last_slab_points
-    spatial_dict[n] = trn_dict
-
-    return spatial_dict, len(slab_lens_b)
 
 def main():
     """Main routine for btrn.py
@@ -433,14 +414,14 @@ def main():
     # subdirectories from previous runs (files are ok)
     data_path = os.path.join(run_path,'data')
     if not os.path.isdir(data_path):
-	os.mkdir(data_path)
+        os.mkdir(data_path)
     else:
-	os.chdir(data_path)
         for el in os.listdir(data_path):
-	    if os.path.isdir(el):
+	    el_path = os.path.join(data_path, el)
+            if os.path.isdir(el_path):
                 raise Exception("Data directory {} has subdirectories! Remove \
-		                 these and continue.".format(data_path))
-	
+                                 these and continue.".format(data_path))
+    
     # Initialize the transportresponse class
     one_d_tool = transportresponse(run_path, data_path, \
                                    args.environment, args.response)
@@ -450,7 +431,8 @@ def main():
 
     # ToDo:  Use a different function
     material_name_dict = tag_utils.get_fnames_for_vol(uwuw_file)
-    ray_tuples = load_directions(args.ray_dir_file, args.num_ray_dirs)
+    rays_file  = os.path.join(os.getcwd(), args.ray_dir_file)
+    ray_tuples = load_directions(rays_file, args.num_ray_dirs)
 
     # The default starting point is 0,0,0
     ref_point = args.ref_point
@@ -459,14 +441,13 @@ def main():
         msg = "Exiting program: The reference point is in the graveyard!"
         sys.exit(msg)
     
-    # Test function version
     index = 0
     for uvw in ray_tuples:
         index = index + 1
-	spatial_dict, number_slabs_to_reference_point =  \
-	             createTransportDictionary(uvw, graveyard_vol, start_vol, \
-		                               ref_point, material_name_dict)
-        one_d_tool.execute(spatial_dict)
+        spatial_tuples, number_slabs_to_reference_point =  \
+                 createTransportDictionary(uvw, graveyard_vol, start_vol, \
+                                       ref_point, material_name_dict)
+        one_d_tool.execute(spatial_tuples)
         one_d_tool.collect_results(data_path, uvw, number_slabs_to_reference_point, index) 
     return 
    
