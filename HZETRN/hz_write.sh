6d1e3bf81ae4ea6c4540b86f50f7dd369690b615
Julie C. Zachman <zachman@engr.wisc.edu>
2015-07-13 10:13:58 -0500

    Add some doc and refs

--- HZETRN/hz_write.sh
+++ HZETRN/hz_write.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+#
+# References
+# http://stackoverflow.com/questions/16483119/example-of-how-to-use-getopts-in-bash
+# http://www.theunixschool.com/2012/08/getopts-how-to-pass-command-line-options-shell-script-Linux.html
+# http://www.unix.com/man-page/all/1/getopts/
+# http://stackoverflow.com/questions/7529856/bash-getopts-retrieving-multiple-variables-from-one-flag
+# 
 usage() { echo "Usage: $0 [-f <infile>] [-r <response>] [-p <particle>] [-g <group>] [-d] [-o <outfile>]" 1>&2; exit 1; }
 
+# d is optional
 while getopts ":f:r:p:g:do:" clp; do
     case "${clp}" in 
         f)
@@ -34,14 +41,14 @@ if [ -z "${f}" ] || [ -z "${resp}" ] || [ -z "${ple}" ] || [ -z "${o}" ]; then
     usage
 fi
 
-echo "f = ${f}"
-echo "r = ${resp[@]}"
+echo "Input data file f = ${f}"
+echo "Response r = ${resp[@]}"
 echo "Or:"
 for val in "${resp[@]}"; do 
     echo "  - $val"
 done
-echo "p = ${ple[@]}"
+echo "Particle p = ${ple[@]}"
 if [ ! -z "$d" ]; then
-   echo "d on"
+   echo "Write direction d on"
 fi
-echo "o = ${o}"
+echo "Output file o = ${o}"
