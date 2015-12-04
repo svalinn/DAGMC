e9fe0901f8066246354dc18275a96e16206d0b37
pshriwise <shriwise@wisc.edu>
2015-12-04 15:40:43 -0600

    mw_funcs now builds.

--- make_watertight/cleanup.hpp
+++ make_watertight/cleanup.hpp
@@ -1,12 +1,12 @@
 #ifndef CLEANUP_HPP
 #define CLEANUP_HPP
 
-#include "MBCore.hpp"
+#include "moab/Core.hpp"
 #include "gen.hpp"
 #include "arc.hpp"
 #include "zip.hpp"
 
-MBInterface *MBI();
+moab::Interface *MBI();
 namespace cleanup {
 
   /// The obbtrees are no longer valid because the triangles have been altered.
@@ -15,16 +15,16 @@ namespace cleanup {
   ///  -Surface/volume set handles are added to the root meshset.
   /// Somehow, delete the old tree without deleting the
   /// surface and volume sets, then build a new tree.
-  MBErrorCode remove_obb_tree(bool verbose = false);
+  moab::ErrorCode remove_obb_tree(bool verbose = false);
 
-  MBErrorCode delete_small_edge_and_tris( const MBEntityHandle vert0, 
-                                          MBEntityHandle &vert1, const double tol);
-  MBErrorCode delete_small_edges( const MBRange &surfaces, const double MERGE_TOL);
+  moab::ErrorCode delete_small_edge_and_tris( const moab::EntityHandle vert0, 
+                                          moab::EntityHandle &vert1, const double tol);
+  moab::ErrorCode delete_small_edges( const moab::Range &surfaces, const double MERGE_TOL);
 
   /// Lots of edges have been created but are no longer needed.
   /// Delete edges that are not in curves. These should be the only edges
   /// that remain. This incredibly speeds up the watertight_check tool (100x?).
-  MBErrorCode cleanup_edges( MBRange curve_meshsets );  
+  moab::ErrorCode cleanup_edges( moab::Range curve_meshsets );  
 }
 
 #endif
