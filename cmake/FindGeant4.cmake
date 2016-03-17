4c3f142764c1530dc442d4400d08e31124b29bfc
Lucas John Jacobson <ljjacobson@wisc.edu>
2016-03-16 19:30:48 -0500

    rewrote cmake for mcnp5, pyne, tally, uwuw, common

--- cmake/FindGeant4.cmake
+++ cmake/FindGeant4.cmake
@@ -1,25 +1,28 @@
-# Try to find MOAB
+# Try to find Geant4
 #
 # Once done this will define
 #
-#  MOAB_FOUND - system has MOAB
-#  MOAB_INCLUDE_DIRS - the MOAB include directory
-#  MOAB_LIBRARIES - Link these to use MOAB
-#  MOAB_DEFINITIONS - Compiler switches required for using MOAB
+#  GEANT4_FOUND - System has Geant4
+#  GEANT4_INCLUDE_DIR - The Geant4 include dir
+#  GEANT4_LIBRARIES - In case they are ever needed
 
-# GEANT4_FOUND - System has Geant4
-# GEANT4_INCLUDE_DIR - The Geant4 include dir
-# GEANT4_LIBRARIES - In case they are ever needed
+find_path(GEANT4_CMAKE_CONFIG
+          NAMES Geant4Config.cmake
+          HINTS ${GEANT4_DIR}
+                ${GEANT4_DIR}/lib/Geant4-10.0.0
+                ${GEANT4_DIR}/lib/Geant4-10.0.1
+                ${GEANT4_DIR}/lib/Geant4-10.0.2
+                ${GEANT4_DIR}/lib/Geant4-10.0.3
+                ${GEANT4_DIR}/lib/Geant4-10.0.4
+                ${GEANT4_DIR}/lib/Geant4-10.1.0
+                ${GEANT4_DIR}/lib/Geant4-10.1.1
+                ${GEANT4_DIR}/lib/Geant4-10.1.2
+                ${GEANT4_DIR}/lib/Geant4-10.1.3
+                ${GEANT4_DIR}/lib/Geant4-10.2.0
+          PATHS ENV LD_LIBRARY_PATH
+          PATH_SUFFIXES lib Lib
+          NO_DEFAULT_PATH)
 
-find_path( GEANT4_CMAKE_CONFIG NAMES Geant4Config.cmake
-  HINTS ${GEANT4_DIR}/lib/Geant4-10.0.2 ${GEANT4_DIR}/lib/Geant4-10.1.2 ${GEANT4_DIR}
-  PATHS ENV LD_LIBRARY_PATH
-  PATH_SUFFIXES lib Lib
-  NO_DEFAULT_PATH
-  )
+message(STATUS "Found Geant4 in ${GEANT4_CMAKE_CONFIG}")
 
-MESSAGE ( STATUS "Found Geant4 in ${GEANT4_CMAKE_CONFIG}" )
-
-INCLUDE ( ${GEANT4_CMAKE_CONFIG}/Geant4Config.cmake )
-
-
+include(${GEANT4_CMAKE_CONFIG}/Geant4Config.cmake)
