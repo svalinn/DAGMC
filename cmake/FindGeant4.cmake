441447c947fcdca83cfd98be4bc54afc6fdb59a0
lucas <ljjacobson@wisc.edu>
2016-03-24 19:45:01 -0500

    updated cmake for geant4

--- cmake/FindGeant4.cmake
+++ cmake/FindGeant4.cmake
@@ -2,27 +2,21 @@
 #
 # Once done this will define
 #
-#  GEANT4_FOUND - System has Geant4
-#  GEANT4_INCLUDE_DIR - The Geant4 include dir
-#  GEANT4_LIBRARIES - In case they are ever needed
+#  Geant4_USE_FILE - CMake config for Geant4
+#  Geant4_INCLUDE_DIRS - Geant4 include directories
+#  Geant4_LIBRARIES - In case they are ever needed
 
+# Try to find Geant4Config.cmake
+execute_process(COMMAND find ${GEANT4_DIR}/lib -type d OUTPUT_VARIABLE SEARCH_DIRS)
+string(REPLACE "\n" ";" SEARCH_DIRS ${SEARCH_DIRS})
 find_path(GEANT4_CMAKE_CONFIG
           NAMES Geant4Config.cmake
-          HINTS ${GEANT4_DIR}
-                ${GEANT4_DIR}/lib/Geant4-10.0.0
-                ${GEANT4_DIR}/lib/Geant4-10.0.1
-                ${GEANT4_DIR}/lib/Geant4-10.0.2
-                ${GEANT4_DIR}/lib/Geant4-10.0.3
-                ${GEANT4_DIR}/lib/Geant4-10.0.4
-                ${GEANT4_DIR}/lib/Geant4-10.1.0
-                ${GEANT4_DIR}/lib/Geant4-10.1.1
-                ${GEANT4_DIR}/lib/Geant4-10.1.2
-                ${GEANT4_DIR}/lib/Geant4-10.1.3
-                ${GEANT4_DIR}/lib/Geant4-10.2.0
-          PATHS ENV LD_LIBRARY_PATH
-          PATH_SUFFIXES lib Lib
+          PATHS ${SEARCH_DIRS}
           NO_DEFAULT_PATH)
+if (${GEANT4_CMAKE_CONFIG} MATCHES GEANT4_CMAKE_CONFIG-NOTFOUND)
+  message(FATAL_ERROR "Geant4Config.cmake not found. Make sure to set -DGEANT4_DIR when running CMake.")
+endif (${GEANT4_CMAKE_CONFIG} MATCHES GEANT4_CMAKE_CONFIG-NOTFOUND)
+message(STATUS "Found Geant4Config.cmake at ${GEANT4_CMAKE_CONFIG}")
 
-message(STATUS "Found Geant4 in ${GEANT4_CMAKE_CONFIG}")
-
+# Geant4 CMake config
 include(${GEANT4_CMAKE_CONFIG}/Geant4Config.cmake)
