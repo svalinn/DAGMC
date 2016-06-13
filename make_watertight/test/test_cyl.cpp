1af561ef0d3a9a144999a18cb81512b250fb5195
pshriwise <shriwise@wisc.edu>
2016-06-13 15:21:07 -0500

    Factoring out any test functions that aren't cylinder-specific for use in other test models.

--- make_watertight/test/test_cyl.cpp
+++ make_watertight/test/test_cyl.cpp
@@ -20,123 +20,18 @@
 #include "moab/Range.hpp"
 #include "moab/Skinner.hpp"
 
+#include "test_funcs.hpp"
+
 #include "mw_func.hpp"
 #include "cw_func.hpp"
 #include "gen.hpp"
 #include "arc.hpp"
 #include "zip.hpp"
 
-moab::ErrorCode move_vert( moab::EntityHandle vertex, double dx, double dy, double dz, bool verbose = false );
-moab::ErrorCode rand_vert_move( moab::EntityHandle vertex, double tol, bool verbose = false);
+// moves a vertex along the theta direction of the cylinder
 moab::ErrorCode move_vert_theta( moab::EntityHandle vertex, double tolerance, bool verbose = false);
+// moves a vertex outward from the center of the cylinter
 moab::ErrorCode move_vert_R ( moab::EntityHandle vertex, double tol, bool verbose = false ) ;
-
-moab::Interface *MBI();
-
-/// struct to hold coordinates of skin edge, it's surface id, and a matched flag
-struct coords_and_id {
-  double x1;
-  double y1;
-  double z1;
-  double x2;
-  double y2;
-  double z2;
-  int  surf_id;
-  bool matched;
-  moab::EntityHandle vert1;
-  moab::EntityHandle vert2;
-};
-
-/// takes an entity handle vertex, gets the original coordinates, changes and resets the vertex coordinates in the mesh
-moab::ErrorCode move_vert( moab::EntityHandle vertex, double dx, double dy, double dz, bool verbose) {
-
- moab::ErrorCode result;
- 
- //get coordinates from the mesh
- double coords[3];
- result= MBI()-> get_coords( &vertex , 1 , coords );
- if(gen::error(moab::MB_SUCCESS!=result, "could not get the vertex coordinates")) return result; 
-
- if(verbose)
- {
-  //original coordinates
-  std::cout << std::endl << "Original Coordinates" << std::endl;
-  std::cout << "x = " << coords[0] << std::endl;
-  std::cout << "y = " << coords[1] << std::endl;
-  std::cout << "z = " << coords[2] << std::endl;
- }
-  coords[0]+=dx;
-  coords[1]+=dy;
-  coords[2]+=dz;
-  
- if(verbose)
- {
-  //altered coordinates
-  std::cout << std::endl << "Modified Coordinates" << std::endl;
-  std::cout << "x = " << coords[0] << std::endl;
-  std::cout << "y = " << coords[1] << std::endl;
-  std::cout << "z = " << coords[2] << std::endl;
- }
- 
-  //write new coordinates to the mesh
-  result=MBI()-> set_coords( &vertex, 1, coords);
-  if (gen::error(moab::MB_SUCCESS!=result, "could not set the vertex coordinates")) return result;
-
-return moab::MB_SUCCESS;
-}
-
-/// takes an entity handle vertex, gets the original coordinates, changes and resets the vertex coordinates in the mesh
-/// setup to move the vert no further than the faceting tolerance
-moab::ErrorCode rand_vert_move( moab::EntityHandle vertex, double tol, bool verbose) {
-
- moab::ErrorCode result;
- 
- //get coordinates from the mesh
- double coords[3];
- result= MBI()-> get_coords( &vertex , 1 , coords );
- if(gen::error(moab::MB_SUCCESS!=result, "could not get the vertex coordinates")) return result; 
-
- // get random values for the changes in x,y,z
- double dx,dy,dz;
- dx=rand(); dy=rand(); dz=rand();
-
- double mag= sqrt(pow(dx,2)+pow(dy,2)+pow(dz,2));
-
- // set the change in the vertex to be a unit vector
- dx/=mag; dy/=mag; dz/=mag;
-
- // set the change in the vertex to be something slightly less than the facet tolerance
- dx*=tol*0.9; dy*=tol*0.9; dz*=tol*0.9;
-
-
- if(verbose)
- {
-  //original coordinates
-  std::cout << std::endl << "Original Coordinates" << std::endl;
-  std::cout << "x = " << coords[0] << std::endl;
-  std::cout << "y = " << coords[1] << std::endl;
-  std::cout << "z = " << coords[2] << std::endl;
- }
-  coords[0]+=dx;
-  coords[1]+=dy;
-  coords[2]+=dz;
-
- if(verbose)
- {
-  //altered coordinates
-  std::cout << std::endl << "Modified Coordinates" << std::endl;
-  std::cout << "x = " << coords[0] << std::endl;
-  std::cout << "y = " << coords[1] << std::endl;
-  std::cout << "z = " << coords[2] << std::endl;
- }
- 
-  //write new coordinates to the mesh
-  result=MBI()-> set_coords( &vertex, 1, coords);
-  if (gen::error(moab::MB_SUCCESS!=result, "could not set the vertex coordinates")) return result;
-
-return moab::MB_SUCCESS;
-}
-
 /// moves a vertex along the rim of the cylinder in the theta direction a distance equal to the faceting_tolerance
 moab::ErrorCode move_vert_theta( moab::EntityHandle vertex, double tolerance, bool verbose) {
 
@@ -230,63 +125,8 @@ return moab::MB_SUCCESS;
 }
 
 
-/// appends "_mod" to the original file name and writes to a new .h5m 
-moab::ErrorCode write_mod_file( std::string filename ) {
-
- moab::ErrorCode result;
- std::string output_filename = filename + "_mod.h5m";
- //write file
- result=MBI()->write_mesh(output_filename.c_str());
- if(gen::error(moab::MB_SUCCESS!=result,"could not write the mesh to output_filename")) return result; 
-
-  return moab::MB_SUCCESS;
-}
 
 
-// used to clear all mesh data and reload the file as original
-moab::ErrorCode reload_mesh(const char* filename,  moab::EntityHandle &meshset, bool debug = false) {
-
-  moab::ErrorCode result;
-
-  // delete meshset
-  result= MBI() -> delete_mesh();
-  if(gen::error(moab::MB_SUCCESS!=result, "could not delete the mesh set")) return result;
-
-
-
-  // re-initialize meshset
-  result= MBI() -> create_meshset(moab::MESHSET_SET, meshset);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not create the new meshset")) return result; 
- 
-  //reload the file
-  result = MBI() -> load_file(filename, &meshset);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not re-load the file into the mesh set")) return result; 
-
-  //check that something was loaded into the meshset
-  int num_meshsets;   
-
-  result= MBI() -> num_contained_meshsets ( meshset, &num_meshsets);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not get the number of meshsets")) return result; 
-  
-  if(debug) std::cout << "number of mesh sets after reload = " << num_meshsets << std::endl;
-
-  if(num_meshsets==0) return moab::MB_FAILURE;
-
-  return result;
-}
-
-
-/// bumps the last vertex in the model by the x,y,z values given to the problem 
-moab::ErrorCode single_vert_bump( moab::Range verts, double bump_dist_x, double bump_dist_y, double bump_dist_z, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-  moab::EntityHandle vertex=verts.back();
-  //move the desired vertex by the allotted distance
-  result = move_vert( vertex, bump_dist_x, bump_dist_y, bump_dist_z );
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move single vert")) return result;
-
-  return moab::MB_SUCCESS;
-}
 
 /// bumps the last vertex in the cylinder model in the R direction
 moab::ErrorCode single_vert_bump_R( moab::Range verts, double facet_tol, bool verbose = false ) {
@@ -300,46 +140,6 @@ moab::ErrorCode single_vert_bump_R( moab::Range verts, double facet_tol, bool ve
   return moab::MB_SUCCESS;
 }
 
-/// moves the last two verticies in the model the same distance in x, y, and z 
-moab::ErrorCode locked_pair_bump( moab::Range verts, double bump_dist_x, double bump_dist_y, double bump_dist_z,  std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-1);
- 
-  //move the desired verticies by the allotted distance(s)
-  result = move_vert( vertex1, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = move_vert( vertex2, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-
-  return moab::MB_SUCCESS;
-}
-/// selects a random pair of verticies from verts and moves them in random directions some distance less than the faceting tolerance
-moab::ErrorCode rand_locked_pair_bump_rand( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  //select random verticies from verts
-  int number_of_verts = verts.size();
-  double num = rand()/static_cast<double>(RAND_MAX);
- 
-  int index = 1+static_cast<int>(num*(number_of_verts-1));
-  //std::cout << "index = " << index << std::endl;
-
-  moab::EntityHandle vertex1=(verts.back()-index);
-  moab::EntityHandle vertex2=(verts.back()-index-1);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = rand_vert_move( vertex1, facet_tol , verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = rand_vert_move( vertex2, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
 
 /// selects a random pair of verticies and moves them along theta a distance less than the faceting tolerance
 moab::ErrorCode rand_locked_pair_bump_theta( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
@@ -364,59 +164,8 @@ moab::ErrorCode rand_locked_pair_bump_theta( moab::Range verts, double facet_tol
 
   return moab::MB_SUCCESS;
 }
-/// selects a random pair of adjacent verticies and bumps them in x, y, and z 
-moab::ErrorCode rand_locked_pair_bump( moab::Range verts, double bump_dist_x, double bump_dist_y, double bump_dist_z,  std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  //select random verticies from verts
-  int number_of_verts = verts.size();
-  double num = rand()/static_cast<double>(RAND_MAX);
- 
-  int index = 1+static_cast<int>(num*(number_of_verts-1));
-  //std::cout << "index = " << index << std::endl;
-
-  moab::EntityHandle vertex1=(verts.back()-index);
-  moab::EntityHandle vertex2=(verts.back()-index-1);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = move_vert( vertex1, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = move_vert( vertex2, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
-/// moves the last two verticies in the model in the same direction some distance less than the faceting tolerance
-moab::ErrorCode locked_pair_bump_rand( moab::Range verts, double facet_tol,  std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-1);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = rand_vert_move( vertex1, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = rand_vert_move( vertex2, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
 
 
-/// moves the last vertex in the model in a random direction by a distance less than the faceting tolerance
-moab::ErrorCode rand_vert_bump( moab::Range verts, double facet_tol, std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-  moab::EntityHandle vertex = verts.back();
-  //move the desired vertex by the allotted distance
-  result = rand_vert_move( vertex, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move single vert")) return result;
-
-
-  return moab::MB_SUCCESS;
-}
 
 // FOR CYLINDER TESTING ONLY 
 /// moves the last vertex in the model along the curve of the cylinder some distance bump distance theta
@@ -489,23 +238,6 @@ moab::ErrorCode locked_pair_move_theta( moab::Range verts, double tolerance, std
   return moab::MB_SUCCESS;
 }
 
-/// moves the third to last and the last vertices in the model the same distance in x, y, and z
-moab::ErrorCode adjplone_locked_pair_bump( moab::Range verts, double bump_dist_x, double bump_dist_y, double bump_dist_z,  std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-2);
- 
-  //move the desired verticies by the allotted distance(s)
-  result = move_vert( vertex1, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = move_vert( vertex2, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-
-  return moab::MB_SUCCESS;
-}
 
 /// moves the third to last and the last verticies in the model in theta the same distance along theta equal to the faceting tolerance
 moab::ErrorCode adjplone_locked_pair_bump_theta( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
@@ -525,96 +257,10 @@ moab::ErrorCode adjplone_locked_pair_bump_theta( moab::Range verts, double facet
   return moab::MB_SUCCESS;
 }
 
-/// moves the third to last and the last verticies in the model in rand directions some distance less than the facet_tolerance
-moab::ErrorCode adjplone_locked_pair_bump_rand( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-2);
- 
-  //move the desired verticies by the allotted distance(s)
-  result = rand_vert_move( vertex1, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = rand_vert_move( vertex2, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
 
 
-  return moab::MB_SUCCESS;
-}
 
 
-/// selects a random pair of adjacent verticies and bumps them the same distance in x, y, and z
-moab::ErrorCode nonadj_locked_pair_bump( moab::Range verts, double bump_dist_x, double bump_dist_y, double bump_dist_z,  std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  //select random verticies from verts
-  int number_of_verts = verts.size();
-  double num = rand()/static_cast<double>(RAND_MAX);
-  //std::cout << "max index = " << (number_of_verts-2)<< std::endl;
-  int index = static_cast<int>(num*((number_of_verts-2)));
-  //std::cout << "index = " << index << std::endl;
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-index);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = move_vert( vertex1, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = move_vert( vertex2, bump_dist_x, bump_dist_y, bump_dist_z, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
-
-/// selects a random pair of adjacent verticies and bumps them along the theta direction a distance equal to the faceting tolerance
-moab::ErrorCode nonadj_locked_pair_bump_theta( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  //select random verticies from verts
-  int number_of_verts = verts.size();
-  double num = rand()/static_cast<double>(RAND_MAX);
-  //std::cout << "max index = " << (number_of_verts-2)<< std::endl;
-  int index = static_cast<int>(num*((number_of_verts-2)));
-  //std::cout << "index = " << index << std::endl;
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-index);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = move_vert_theta( vertex1, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = move_vert_theta( vertex2, facet_tol , verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
-
-/// selects a random pair of adjacent verticies and bumps them along the theta direction a distance equal to the faceting tolerance
-moab::ErrorCode nonadj_locked_pair_bump_rand( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
- 
-  moab::ErrorCode result; 
-
-  //select random verticies from verts
-  int number_of_verts = verts.size();
-  double num = rand()/static_cast<double>(RAND_MAX);
-  //std::cout << "max index = " << (number_of_verts-2)<< std::endl;
-  int index = static_cast<int>(num*((number_of_verts-2)));
-  //std::cout << "index = " << index << std::endl;
-
-  moab::EntityHandle vertex1=verts.back();
-  moab::EntityHandle vertex2=(verts.back()-index);
-  
-  //move the desired verticies by the allotted distance(s)
-  result = rand_vert_move( vertex1, facet_tol, verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
-  result = rand_vert_move( vertex2, facet_tol , verbose);
-  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
-
-  return moab::MB_SUCCESS;
-}
 
 moab::ErrorCode locked_pair_bump_R( moab::Range verts, double facet_tol,  std::string root_name, bool verbose = false ) {
  
@@ -698,7 +344,30 @@ moab::ErrorCode nonadj_locked_pair_bump_R( moab::Range verts, double facet_tol ,
 
   return moab::MB_SUCCESS;
 }
+
+/// selects a random pair of adjacent verticies and bumps them along the theta direction a distance equal to the faceting tolerance
+moab::ErrorCode nonadj_locked_pair_bump_theta( moab::Range verts, double facet_tol , std::string root_name, bool verbose = false ) {
  
+  moab::ErrorCode result; 
+
+  //select random verticies from verts
+  int number_of_verts = verts.size();
+  double num = rand()/static_cast<double>(RAND_MAX);
+  //std::cout << "max index = " << (number_of_verts-2)<< std::endl;
+  int index = static_cast<int>(num*((number_of_verts-2)));
+  //std::cout << "index = " << index << std::endl;
+
+  moab::EntityHandle vertex1=verts.back();
+  moab::EntityHandle vertex2=(verts.back()-index);
+  
+  //move the desired verticies by the allotted distance(s)
+  result = move_vert_theta( vertex1, facet_tol, verbose);
+  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex1")) return result;
+  result = move_vert_theta( vertex2, facet_tol , verbose);
+  if(gen::error(moab::MB_SUCCESS!=result, "could not move vertex2")) return result;
+
+  return moab::MB_SUCCESS;
+}
 
 int main(int argc, char **argv)
 {
@@ -1944,8 +1613,4 @@ std::cout << "NON-ADJACENT LOCKED PAIR TESTS" << std::endl;
 
 }
 
-moab::Interface* MBI() {
- static moab::Core instance;
- return &instance;
-}
 
