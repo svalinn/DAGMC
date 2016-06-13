209a7d03bcb9b56d8a53d9015fe5e8a021276776
Lucas John Jacobson <ljjacobson@wisc.edu>
2016-06-13 13:45:29 -0500

    Make sure all LCAD cell entries have an imp:<particle> card if any of them do

--- mcnp/mcnp_funcs.cpp
+++ mcnp/mcnp_funcs.cpp
@@ -476,16 +476,38 @@ void write_lcad_old(std::ofstream &lcadfile)
   int num_surfs = DAG->num_entities( 2 );
 
   int cmat = 0;
-  double crho, cimp = 1.0;
+  double crho = 0;
+  double cimp_n = 0, cimp_p = 0, cimp_e = 0;
+
+  // Detect which importances are used so all cells, including implicit
+  // complement and graveyard, have these importances
+  bool imp_n_needed = false, imp_p_needed = false, imp_e_needed = false;
+  for( int i = 1; i <= num_cells; ++i ) {
+    moab::EntityHandle vol = DAG->entity_by_index( 3, i );
+    if( DAG->has_prop( vol, "imp.n" )) {
+      imp_n_needed = true;
+      cimp_n = 1;
+    }
+    if( DAG->has_prop( vol, "imp.p" )) {
+      imp_p_needed = true;
+      cimp_p = 1;
+    }
+    if( DAG->has_prop( vol, "imp.e" )) {
+      imp_e_needed = true;
+      cimp_e = 1;
+    }
+  }
 
   // write the cell cards
   for( int i = 1; i <= num_cells; ++i ) {
 
     moab::EntityHandle vol = DAG->entity_by_index( 3, i );
     int cellid = DAG->id_by_index( 3, i );
-    // set default importances for p and e to negative, indicating no card should be printed.
-    double imp_n = 1, imp_p = -1, imp_e = -1;
 
+    // Set default importances
+    double imp_n = cimp_n, imp_p = cimp_p, imp_e = cimp_e;
+
+    // Get importances from DAGMC
     if( DAG->has_prop( vol, "imp.n" )) {
       get_real_prop( vol, cellid, "imp.n", imp_n );
     }
@@ -498,37 +520,53 @@ void write_lcad_old(std::ofstream &lcadfile)
       get_real_prop( vol, cellid, "imp.e", imp_e );
     }
 
-    lcadfile << cellid << " ";
+    // If no importances specified, default to neutron mode
+    if ( ! imp_n_needed && ! imp_p_needed && ! imp_e_needed ) {
+      imp_n_needed = true;
+      cimp_n = 1;
+      imp_n = 1;
+    }
+
+    lcadfile << cellid;
 
     bool graveyard = DAG->has_prop( vol, "graveyard" );
 
     if( graveyard ) {
-      lcadfile << " 0 imp:n=0";
+      lcadfile << " 0";
+      if( imp_n_needed ) lcadfile << " imp:n=0";
+      if( imp_p_needed ) lcadfile << " imp:p=0";
+      if( imp_e_needed ) lcadfile << " imp:e=0";
+      lcadfile << " $ graveyard";
       if( DAG->has_prop(vol, "comp") ) {
         // material for the implicit complement has been specified.
         get_int_prop( vol, cellid, "mat", cmat );
         get_real_prop( vol, cellid, "rho", crho );
-        std::cout << "Detected material and density specified for implicit complement: " << cmat <<", " << crho << std::endl;
-        cimp = imp_n;
+        std::cout << "Detected material and density specified for implicit complement: " << cmat << ", " << crho << std::endl;
+        if( imp_n_needed ) cimp_n = imp_n;
+        if( imp_p_needed ) cimp_p = imp_p;
+        if( imp_e_needed ) cimp_e = imp_e;
       }
     } else if( DAG->is_implicit_complement(vol) ) {
-      lcadfile << cmat;
+      lcadfile << " " << cmat;
       if( cmat != 0 ) lcadfile << " " << crho;
-      lcadfile << " imp:n=" << cimp << " $ implicit complement";
+      if( imp_n_needed ) lcadfile << " imp:n=" << cimp_n;
+      if( imp_p_needed ) lcadfile << " imp:p=" << cimp_p;
+      if( imp_e_needed ) lcadfile << " imp:e=" << cimp_e;
+      lcadfile << " $ implicit complement";
     } else {
       int mat = 0;
       get_int_prop( vol, cellid, "mat", mat );
 
       if( mat == 0 ) {
-        lcadfile << "0";
+        lcadfile << " 0";
       } else {
         double rho = 1.0;
         get_real_prop( vol, cellid, "rho", rho );
-        lcadfile << mat << " " << rho;
+        lcadfile << " " << mat << " " << rho;
       }
-      lcadfile << " imp:n=" << imp_n;
-      if( imp_p > 0 ) lcadfile << " imp:p=" << imp_p;
-      if( imp_e > 0 ) lcadfile << " imp:e=" << imp_e;
+      if( imp_n_needed ) lcadfile << " imp:n=" << imp_n;
+      if( imp_p_needed ) lcadfile << " imp:p=" << imp_p;
+      if( imp_e_needed ) lcadfile << " imp:e=" << imp_e;
     }
 
     lcadfile << std::endl;
