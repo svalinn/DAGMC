cb70956dacce832b59781abc52c17fe62332a306
Andrew Davis <davisa@engr.wisc.edu>
2016-07-08 03:38:51 -0500

    bad character

--- .build_moab.sh
+++ .build_moab.sh
@@ -8,7 +8,7 @@ git checkout $MOAB_VERSION
 autoreconf -fi
 mkdir bld
 cd bld
-/./configure --enable-dagmc --enable-shared --disable-debug --enable-optimize --with-hdf5 --prefix=/root/moab
+../configure --enable-dagmc --enable-shared --disable-debug --enable-optimize --with-hdf5 --prefix=/root/moab
 make -j2
 make install
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$root/moab/lib/"
