4ecc36e06cc0389cc8de679df69c5b96b22c8034
pshriwise <shriwise@wisc.edu>
2016-09-14 14:29:34 -0500

    Adding better conditions for when make_watertight regression tests are run and passing environment variable to docker instance.

--- .run_tests.sh
+++ .run_tests.sh
@@ -27,7 +27,7 @@ cd tests
 ./make_watertight_cylinder_tests
 ./make_watertight_cone_tests
 # if this is not a pull request, run regression tests
-if [ ! TRAVIS_PULL_REQUEST ] ; then
+if [ ! -z $TRAVIS_PULL_REQUEST ] && [ $TRAVIS_PULL_REQUEST == "false" ] ; then
     wget $MW_REG_TEST_MODELS_URL -O mw_reg_test_files.tar.gz -o wget.out
     tar xzvf mw_reg_test_files.tar.gz
     ./make_watertight_regression_tests
