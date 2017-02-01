c5ea31e40c7e20ae0c1907c43b9fae78ed40be4d
pshriwise <shriwise@wisc.edu>
2017-02-01 17:14:39 -0600

    Adding methods for getting an element index and finding a signed distance value for a point.

--- sdf.cpp
+++ sdf.cpp
@@ -10,14 +10,53 @@ namespace moab {
 
   double SignedDistanceField::find_sdv(const double pnt[3]) {
 
+    //first figure out what element we're in
     int i,j,k;
-    get_ijk(pnt,i,j,k);
-    
-    
+    get_element_ijk(pnt,i,j,k);
+
+    //now get the data for that element and construct associated points
+    double sdvs[8];
+    sdvs[0] = signed_distance_values[(i)+(j)*dims[0]+(k)*dims[0]*dims[1]];
+    sdvs[1] = signed_distance_values[(i+1)+(j)*dims[0]+(k)*dims[0]*dims[1]];
+    sdvs[2] = signed_distance_values[(i+1)+(j+1)*dims[0]+(k)*dims[0]*dims[1]];
+    sdvs[3] = signed_distance_values[(i)+(j+1)*dims[0]+(k)*dims[0]*dims[1]];
+    sdvs[4] = signed_distance_values[(i)+(j)*dims[0]+(k+1)*dims[0]*dims[1]];
+    sdvs[5] = signed_distance_values[(i+1)+(j)*dims[0]+(k+1)*dims[0]*dims[1]];
+    sdvs[6] = signed_distance_values[(i+1)+(j+1)*dims[0]+(k+1)*dims[0]*dims[1]];
+    sdvs[7] = signed_distance_values[(i)+(j+1)*dims[0]+(k+1)*dims[0]*dims[1]];
+
+    CartVect coords[8];
+    coords[0] = CartVect((i)*step_size,(j)*step_size,(k)*step_size);
+    coords[1] = CartVect((i+1)*step_size,(j)*step_size,(k)*step_size);
+    coords[2] = CartVect((i+1)*step_size,(j+1)*step_size,(k)*step_size);
+    coords[3] = CartVect((i)*step_size,(j+1)*step_size,(k)*step_size);
+    coords[4] = CartVect((i)*step_size,(j)*step_size,(k+1)*step_size);
+    coords[5] = CartVect((i+1)*step_size,(j)*step_size,(k+1)*step_size);
+    coords[6] = CartVect((i+1)*step_size,(j+1)*step_size,(k+1)*step_size);
+    coords[7] = CartVect((i)*step_size,(j+1)*step_size,(k+1)*step_size);
+
+    //interpolate in x
+    double a,b,c,d;
+    a = sdvs[0] + ((sdvs[1]-sdvs[0])/(coords[1][0]-coords[0][0]))*(pnt[0]-coords[0][0]);
+    b = sdvs[2] + ((sdvs[3]-sdvs[2])/(coords[3][0]-coords[2][0]))*(pnt[0]-coords[2][0]);
+    c = sdvs[4] + ((sdvs[5]-sdvs[4])/(coords[5][0]-coords[4][0]))*(pnt[0]-coords[4][0]);
+    d = sdvs[6] + ((sdvs[7]-sdvs[6])/(coords[7][0]-coords[6][0]))*(pnt[0]-coords[6][0]);
+  
+    // interpolate in y
+    double aa,bb;
+    aa = a+((b-a)/(coords[3][1]-coords[1][1]))*(pnt[1]-coords[1][1]);
+    // aa = a*(fabs(pnt[1]-coords[3][1])/precondStep)+b*(fabs(pnt[1]-coords[1][1])/precondStep);
+    bb = c+((d-c)/(coords[7][1]-coords[5][1]))*(pnt[1]-coords[5][1]);
+    // bb = c*(fabs(pnt[1]-coords[7][1])/precondStep)+d*(fabs(pnt[1]-coords[5][1])/precondStep);
+
+    //finally interpolate in z
+    double result = aa+((bb-aa)/(coords[5][2]-coords[1][2]))*(pnt[2]-coords[1][2]);
+
+    return result;
   };
 
   
-  void SignedDistanceField::get_ijk(const double pnt[3], int &i, int &j, int &k) {
+  void SignedDistanceField::get_element_ijk(const double pnt[3], int &i, int &j, int &k) {
     CartVect this_pnt(pnt);
     CartVect llc(lower_left_corner);
     CartVect vec = this_pnt-llc;
@@ -27,10 +66,10 @@ namespace moab {
   };
 
 
-  void SignedDistanceField::get_ijk(const double pnt[3], int &i, int &j, int &k, bool &outside) {
-    get_ijk(pnt, i, j, k);
-    if( i < 0 || j < 0 || k < 0 || i > field_dims[0]-2 || j > field_dims[1]-2 || k > field_dims[2]-2) {
-      outside = true;
+  void SignedDistanceField::get_element_ijk(const double pnt[3], int &i, int &j, int &k, bool &outside) {
+    get_element_ijk(pnt, i, j, k);
+    if( i < 0 || j < 0 || k < 0 || i > dims[0]-2 || j > dims[1]-2 || k > dims[2]-2) {
+    outside = true;
      }
   };
 
--- sdf.hpp
+++ sdf.hpp
@@ -15,26 +15,32 @@ public:
   // reference location 
   double lower_left_corner[3];
   // size of data in each dimension
-  int field_dims[3];
+  int dims[3];
   // distance between data points
   double step_size;
 
   inline void set_dims(int x_steps, int y_steps, int z_steps) {
-    field_dims[0] = x_steps; field_dims[1] = y_steps; field_dims[2] = z_steps;
+    dims[0] = x_steps; dims[1] = y_steps; dims[2] = z_steps;
   }
 
+  inline void set_corner(double x_min, double y_min, double z_min) {
+    lower_left_corner[0] = x_min;
+    lower_left_corner[1] = y_min;
+    lower_left_corner[2] = z_min;
+  };
+  
   inline void set_step(double step) { step_size = step; }
 
   inline void set_data(std::vector<double> data) {
-    assert(data.size() == field_dims[0]*field_dims[1]*field_dims[2]);
+    assert(data.size() == dims[0]*dims[1]*dims[2]);
     signed_distance_values = data;
   }
 
   double find_sdv(const double pnt[3]);
 
   // gets the mesh element for this point
-  void get_ijk(const double pnt[3], int &i, int &j, int &k);
-  void get_ijk(const double pnt[3], int &i, int &j, int &k, bool &outside);
+  void get_element_ijk(const double pnt[3], int &i, int &j, int &k);
+  void get_element_ijk(const double pnt[3], int &i, int &j, int &k, bool &outside);
 
 };
 
