a91abc68f56b93ad25dfe436c72e32a432566126
Andrew Davis <andrew.davis@wisc.edu>
2017-02-15 14:31:52 -0600

    Sets a default filename if none provided

--- tools/build_obb/build_obb.cpp
+++ tools/build_obb/build_obb.cpp
@@ -23,6 +23,13 @@ int main(int argc, char* argv[])
 
   moab::ErrorCode rval;
 
+  // sets the output filename if none specified
+  if(out_file == "") {
+    int pos = dag_file.find(".h5m");
+    out_file = dag_file.substr(0,pos)+"_obb.h5m";
+    std::cout << "Setting default outfile to be " << out_file << std::endl;
+  }
+  
   // read geometry
   rval = DAG->load_file(dag_file.c_str());
   if (moab::MB_SUCCESS != rval) {
