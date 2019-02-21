bdaa53349cc7ae3b08aabb2dbd0ef5ee092af04d
Lucas John Jacobson <ljacobson64@gmail.com>
2019-02-20 22:47:18 -0600

    Don't build MW regression tests if they won't be run

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -16,17 +16,21 @@ cd ${build_dir}/DAGMC-moab-${moab_version}/src/cmake/test_config
 cmake . -DDAGMC_ROOT=${install_dir}/DAGMC-moab-${moab_version}
 make all test
 
-# Run the unit tests
 cd ${build_dir}/DAGMC-moab-${moab_version}/bld
-make test
 
-# If this is not a pull request, run regression tests
-if [ ! -z ${TRAVIS_PULL_REQUEST} ] && [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
+# If this is not a pull request, get files needed for regression tests
+if [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
+  cd src/make_watertight/tests
   wget ${MW_REG_TEST_MODELS_URL} -O mw_reg_test_files.tar.gz -o wget.out
   tar xzvf mw_reg_test_files.tar.gz
-  ./make_watertight_regression_tests
-  rm -rf *.h5m mw_reg_test_files.tar.gz
-  if [ $? != 0 ]; then
-    exit 1
-  fi
+  cd ../../..
+fi
+
+# Run the unit tests
+make test
+
+# Delete regression test files
+if [ ${TRAVIS_PULL_REQUEST} == "false" ]; then
+  rm -f src/make_watertight/tests/*.h5m
+  rm -f src/make_watertight/tests/mw_reg_test_files.tar.gz
 fi
