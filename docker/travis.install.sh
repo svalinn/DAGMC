06f49239b10482f67b21fef859666207e87ab1a4
Lucas John Jacobson <ljacobson64@gmail.com>
2019-02-20 20:53:07 -0600

    Pass travis variables into scripts

--- docker/travis.install.sh
+++ docker/travis.install.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+# $1: astyle only (OFF, ON)
+# $2: compiler (gcc, clang)
+# $3: moab version (5.1.0, master)
+
 set -e
 
-cd /root/build/${COMPILER}/DAGMC-moab-${MOAB_VERSION}/DAGMC
+astyle_only=$1
+compiler=$2
+moab_version=$3
+
+cd /root/build/${compiler}/DAGMC-moab-${moab_version}/DAGMC
 
 # If only running astyle, don't need to build anything
-if [ "${ASTYLE_ONLY}" == "ON" ]; then
+if [ "${astyle_only}" == "ON" ]; then
   exit 0
 fi
 
 # Only build MOAB master; v5.1.0 is already in the docker image
-if [ "${MOAB_VERSION}" == "master" ]; then
-  bash docker/build_moab.sh ${COMPILER} ${MOAB_VERSION}
+if [ "${moab_version}" == "master" ]; then
+  bash docker/build_moab.sh ${compiler} ${moab_version}
 fi
 
 # Build DAGMC (shared executables)
-bash docker/build_dagmc.sh ${COMPILER} ${MOAB_VERSION} OFF
+bash docker/build_dagmc.sh ${compiler} ${moab_version} OFF
 
 # Build DAGMC (static executables)
-bash docker/build_dagmc.sh ${COMPILER} ${MOAB_VERSION} ON
+bash docker/build_dagmc.sh ${compiler} ${moab_version} ON
--- docker/travis.script.sh
+++ docker/travis.script.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+# $1: astyle only (OFF, ON)
+# $2: compiler (gcc, clang)
+# $3: moab version (5.1.0, master)
+
 set -e
 
-cd /root/build/${COMPILER}/DAGMC-moab-${MOAB_VERSION}/DAGMC
+astyle_only=$1
+compiler=$2
+moab_version=$3
+
+cd /root/build/${compiler}/DAGMC-moab-${moab_version}/DAGMC
 
 # Only run astyle
-if [ "${ASTYLE_ONLY}" == "ON" ]; then
+if [ "${astyle_only}" == "ON" ]; then
   bash docker/run_astyle.sh
   exit 0
 fi
@@ -13,4 +21,4 @@ fi
 # Run the tests
 export TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}
 export MW_REG_TEST_MODELS_URL=${MW_REG_TEST_MODELS_URL}
-bash docker/run_tests.sh ${COMPILER} ${MOAB_VERSION}
+bash docker/run_tests.sh ${compiler} ${moab_version}
