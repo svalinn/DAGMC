a4be6ab37a2bfe853c546d175012474bc2fc0abd
Lucas John Jacobson <ljacobson64@gmail.com>
2019-03-20 17:10:16 -0500

    Consolidate into 1 dockerfile

--- docker/update_docker.sh
+++ docker/update_docker.sh
@@ -4,7 +4,7 @@ set -e
 
 ubuntu_versions="16.04 18.04"
 for ubuntu_version in ${ubuntu_versions}; do
-  docker build -f Dockerfile-Ubuntu-${ubuntu_version} .
-  docker tag `docker images -q | head -1` ljacobson64/dagmc-ci-ubuntu-${ubuntu_version}:latest
-  docker push ljacobson64/dagmc-ci-ubuntu-${ubuntu_version}:latest
+  image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}:latest"
+  docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} .
+  docker push ${image_name}
 done
