bf9e6e6d4965d06f0209092d2eca44a1ab03d526
Patrick Shriwise <pshriwise@gmail.com>
2019-09-14 00:44:15 -0500

    Overlap on edges of triangle. So expensive....

--- src/overlap_check/overlap.cpp
+++ src/overlap_check/overlap.cpp
@@ -60,39 +60,65 @@ check_file_for_overlaps(std::shared_ptr<Interface> MBI,
   dir.normalize();
 
   double bump = 1e-06;
-  int num_verts = all_verts.size();
-  for (int i = 0; i < num_verts; i++) {
-    EntityHandle vert = all_verts[i];
 
-    CartVect coords;
-    rval = MBI->get_coords(&vert, 1, coords.array());
-    MB_CHK_SET_ERR(rval, "Failed to get vertex coordinates for vert: " << vert);
+  Range all_tris;
+  rval = MBI->get_entities_by_type(0, MBTRI, all_tris);
 
-    // move our point slightly off the vertex
-    coords += dir * bump;
+  int num_tris = all_tris.size();
+  for (int tri_idx = 0; tri_idx < num_tris; tri_idx++) {
+    EntityHandle tri = all_tris[tri_idx];
 
-    std::set<int>vols_found;
-    rval = check_location_for_overlap(GQT, all_vols, coords, dir, vols_found);
-    MB_CHK_SET_ERR(rval, "Failed to for overlap at location " << coords);
+    Range tri_verts;
+    rval = MBI->get_connectivity(&tri, 1, tri_verts);
+    MB_CHK_SET_ERR(rval, "Failed to get triangle vertices");
 
-    if (vols_found.size() > 1) {
-        overlap_map[vols_found] = coords;
+    CartVect tri_coords[3];
+    rval = MBI->get_coords(tri_verts, tri_coords[0].array());
+    MB_CHK_SET_ERR(rval, "Failed to get triangle coordinates");
+
+    // create locations for this triangle
+    int num_per_edge = 100;
+
+    std::vector<CartVect> locations;
+    locations.push_back(tri_coords[0]);
+    locations.push_back(tri_coords[1]);
+    locations.push_back(tri_coords[2]);
+
+    for (int i = 0; i < 3; i++) {
+      CartVect edge = tri_coords[i < 2 ? i + 1 : 0] - tri_coords[i];
+      CartVect vert = tri_coords[i];
+      for (int j = 0; j < num_per_edge; j++) {
+        double t = (double)j/(double)num_per_edge;
+        locations.push_back(vert + t * edge);
+      }
     }
 
-    // move our point slightly off the vertex
-    dir *= -1;
-    coords += dir * 2.0 * bump;
+    for (auto& loc : locations) {
 
-    vols_found.clear();
-    rval = check_location_for_overlap(GQT, all_vols, coords, dir, vols_found);
-    MB_CHK_SET_ERR(rval, "Failed to for overlap at location " << coords);
+      // move our point slightly off the vertex
+      loc += dir * bump;
 
-    if (vols_found.size() > 1) {
-        overlap_map[vols_found] = coords;
+      std::set<int>vols_found;
+      rval = check_location_for_overlap(GQT, all_vols, loc, dir, vols_found);
+      MB_CHK_SET_ERR(rval, "Failed to for overlap at location " << loc);
+
+      if (vols_found.size() > 1) {
+        overlap_map[vols_found] = loc;
+      }
+
+      // move our point slightly off the vertex
+      dir *= -1;
+      loc += dir * 2.0 * bump;
+
+      vols_found.clear();
+      rval = check_location_for_overlap(GQT, all_vols, loc, dir, vols_found);
+      MB_CHK_SET_ERR(rval, "Failed to for overlap at location " << loc);
+
+      if (vols_found.size() > 1) {
+        overlap_map[vols_found] = loc;
+      }
     }
-
-    prog_bar.set_value(100.0 * (double) i / (double) num_verts);
-
+    prog_bar.set_value(100.0 * (double) tri_idx / (double) num_tris);
   }
 
   return MB_SUCCESS;
