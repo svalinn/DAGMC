3a1cae9edb435b85806ed3a6f7f535f0e7530d99
Patrick Shriwise <pshriwise@gmail.com>
2020-03-09 05:54:28 -0500

    Correction to double down repo name

--- docker/build_double_down.sh
+++ docker/build_double_down.sh
@@ -7,8 +7,8 @@ source ${docker_env}
 rm -rf ${double_down_build_dir}/bld ${double_down_install_dir}
 mkdir -p ${double_down_build_dir}/bld
 cd ${double_down_build_dir}
-git clone https://github.com/pshriwise/double_down
-ln -snf double_down src
+git clone https://github.com/pshriwise/double-down
+ln -snf double-down src
 /bin/bash ${double_down_build_dir}/src/ci/embree-install.sh
 cd bld
 LD_LIBRARY_PATH=${moab_install_dir}/lib:$HOME/EMBREE/lib/:$LD_LIBRARY_PATH \
