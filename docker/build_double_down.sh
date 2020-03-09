b3ce8142eb1bbfee436c57e7673d376f7bb6f62e
Patrick Shriwise <pshriwise@gmail.com>
2020-03-09 10:55:11 -0500

    Testing double-down separately.

--- docker/build_double_down.sh
+++ docker/build_double_down.sh
@@ -14,6 +14,5 @@ cd bld
 LD_LIBRARY_PATH=${moab_install_dir}/lib:$HOME/EMBREE/lib/:$LD_LIBRARY_PATH \
 cmake ../src -DCMAKE_INSTALL_PREFIX=${double_down_install_dir}
 make -j${jobs}
-make test
 make install
 cd
