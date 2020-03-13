063950ef79784bf7515437c0c8a9ac2a616d72bb
Patrick Shriwise <pshriwise@gmail.com>
2020-03-13 10:07:01 -0500

    Run dagsolid test explicitly.

---
+++ docker/travis.tests.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+source ${docker_env}
+
+cd ${dagmc_build_dir}/DAGMC
+
+# clean out config test directory for next build
+git clean -dxf .
+
+# Test DAGMC CMake configuration file
+cd ${dagmc_build_dir}/DAGMC/cmake/test_config
+cmake . -DDAGMC_ROOT=${dagmc_install_dir}
+make all test
+
+cd ${dagmc_build_dir}/bld
+
+# If this is not a pull request, get files needed for regression tests
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  cd src/make_watertight/tests
+  wget ${MW_REG_TEST_MODELS_URL} -O mw_reg_test_files.tar.gz -o wget.out
+  tar xzvf mw_reg_test_files.tar.gz
+  cd ../../..
+fi
+
+# Run the unit tests
+cd ./src/geant4/tests/
+./dagsolid_unit_tests
+cd -
+make test
+
+# Delete regression test files
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  rm -f src/make_watertight/tests/*.h5m
+  rm -f src/make_watertight/tests/mw_reg_test_files.tar.gz
+fi
