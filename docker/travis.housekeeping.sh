368b787c9ebfc99956f6f7e82c1e17a16a35a48b
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-04-02 17:13:59 -0500

    cleaning

--- docker/travis.housekeeping.sh
+++ docker/travis.housekeeping.sh
@@ -18,8 +18,9 @@ if [ "${TRAVIS_REPO_SLUG}" == "svalinn/DAGMC" ] && \
   fi
 fi
 
+# install package for style check
 apt-get install astyle
-pip install sphinx --force-reinstall
+
 # Run astyle check
 astyle --options=astyle_google.ini \
        --exclude=src/gtest \
@@ -40,5 +41,7 @@ else
   exit 1
 fi
 
+#install package to build docs
+pip install sphinx
 # Build documentation
 make html
