4179318f7abba1722026935aeb1e871dee6ac9bc
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-04-09 23:43:35 -0500

    shuffling folder around

--- .travis_scripts/travis.install.sh
+++ .travis_scripts/travis.install.sh
@@ -8,11 +8,11 @@ cd ${dagmc_build_dir}/DAGMC
 
 # Only build MOAB master and develop; v5.1.0 is already in the docker image
 if [ "${MOAB_VERSION}" == "master" ] || [ "${MOAB_VERSION}" == "develop" ]; then
-  docker/build_moab.sh
+  .travis_scripts/build_moab.sh
 fi
 
 # Build DAGMC (shared executables)
-build_static_exe=OFF docker/build_dagmc.sh
+build_static_exe=OFF .travis_scripts/build_dagmc.sh
 
 # Build DAGMC (static executables)
-build_static_exe=ON docker/build_dagmc.sh
+build_static_exe=ON .travis_scripts/build_dagmc.sh
