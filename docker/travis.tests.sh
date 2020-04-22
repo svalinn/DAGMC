de85aa92d493665331776a70bcdab66ae78c7d46
Patrick Shriwise <pshriwise@gmail.com>
2020-04-22 02:19:11 -0500

    Damn my negligence.

--- docker/travis.tests.sh
+++ docker/travis.tests.sh
@@ -4,7 +4,15 @@ set -e
 
 source ${docker_env}
 
-cd ${dagmc_build_dir}/DAGMC
+cd ${dagmc_build_dir}/bld
+
+# If this is not a pull request, get files needed for regression tests
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  cd src/make_watertight/tests
+  wget ${MW_REG_TEST_MODELS_URL} -O mw_reg_test_files.tar.gz -o wget.out
+  tar xzvf mw_reg_test_files.tar.gz
+  cd ../../..
+fi
 
 # Build DAGMC and test (shared executables)
 # GEANT4's internal RPATH's aren't quite right,
@@ -12,7 +20,6 @@ cd ${dagmc_build_dir}/DAGMC
 # test to run
 LD_LIBRARY_PATH=${geant4_install_dir}/lib:$LD_LIBRARY_PATH \
 build_static_exe=OFF docker/build_dagmc.sh
-
 make test
 
 # Build DAGMC and test (static executables)
@@ -28,16 +35,6 @@ cmake . -DDAGMC_ROOT=${dagmc_install_dir}
 make all test
 
 cd ${dagmc_build_dir}/bld
-
-# If this is not a pull request, get files needed for regression tests
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  cd src/make_watertight/tests
-  wget ${MW_REG_TEST_MODELS_URL} -O mw_reg_test_files.tar.gz -o wget.out
-  tar xzvf mw_reg_test_files.tar.gz
-  cd ../../..
-fi
-
-make test
 
 # Delete regression test files
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
