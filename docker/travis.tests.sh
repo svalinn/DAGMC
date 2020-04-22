b5105daeb8cd46504c7baada8cd4fddeae66f5e0
Patrick Shriwise <pshriwise@gmail.com>
2020-04-22 10:15:07 -0500

    Moving LD_LIBRARY_PATH setting.

--- docker/travis.tests.sh
+++ docker/travis.tests.sh
@@ -17,12 +17,12 @@ fi
 # so we need to set the LD_LIBRARY_PATH for the
 # test to run
 cd ${dagmc_build_dir}/DAGMC
-LD_LIBRARY_PATH=${geant4_install_dir}/lib:$LD_LIBRARY_PATH \
 build_static_exe=OFF docker/build_dagmc.sh
 
 cd ${dagmc_build_dir}/bld/src/make_watertight/tests/
 for i in /tmp/*.h5m; do ln -sf $i; done
 cd ${dagmc_build_dir}/bld
+LD_LIBRARY_PATH=${geant4_install_dir}/lib:$LD_LIBRARY_PATH \
 CTEST_OUTPUT_ON_FAILURE=1 make test
 
 # Build DAGMC and test (static executables)
