4b8d093df05a6774b9df8ffd864c09a4ee10ad71
Patrick Shriwise <pshriwise@gmail.com>
2020-04-23 21:48:48 -0500

    Enabling shared build testing on CI.

--- CI/travis/tests.sh
+++ CI/travis/tests.sh
@@ -4,14 +4,42 @@ set -ex
 
 source ${docker_env}
 
+# this command may fail due to lack of Geant4 data
+source ${geant4_install_dir}/bin/geant4.sh || true
+
+# If this is not a pull request, get files needed for regression tests
+if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  cd /tmp
+  wget ${MW_REG_TEST_MODELS_URL} -O mw_reg_test_files.tar.gz -o wget.out
+  tar xzvf mw_reg_test_files.tar.gz
+  cd -
+fi
+
 cd ${dagmc_build_dir}/DAGMC
+build_static_exe=OFF docker/build_dagmc.sh
+
+cd ${dagmc_build_dir}/bld/src/make_watertight/tests/
+for i in /tmp/*.h5m; do ln -sf $i; done
+cd ${dagmc_build_dir}/bld
+CTEST_OUTPUT_ON_FAILURE=1 make test
+
+# Build DAGMC and test (static executables)
+cd ${dagmc_build_dir}/DAGMC
+build_static_exe=ON docker/build_dagmc.sh
+
+cd ${dagmc_build_dir}/bld/src/make_watertight/tests/
+for i in /tmp/*.h5m; do ln -sf $i; done
+cd ${dagmc_build_dir}/bld
+CTEST_OUTPUT_ON_FAILURE=1 make test
 
 # clean out config test directory for next build
-git clean -dxf . 
+cd ${dagmc_build_dir}/DAGMC
+git clean -dxf .
 
 # Test DAGMC CMake configuration file
 cd ${dagmc_build_dir}/DAGMC/cmake/test_config
 cmake . -DDAGMC_ROOT=${dagmc_install_dir}
+for i in /tmp/*.h5m; do ln -sf src/make_watertight/tests/$i; done
 make all test
 
 cd ${dagmc_build_dir}/bld
@@ -29,6 +57,6 @@ make test
 
 # Delete regression test files
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  rm -f src/make_watertight/tests/*.h5m
-  rm -f src/make_watertight/tests/mw_reg_test_files.tar.gz
+  rm -f /tmp/*.h5m
+  rm -f /tmp/mw_reg_test_files.tar.gz
 fi
