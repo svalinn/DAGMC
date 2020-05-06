402ceb6b88297388c64d1dccf1158e4ac15e4fae
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-05-06 18:08:28 -0500

    fixing dockerfile

--- scripts/update_docker.sh
+++ scripts/update_docker.sh
@@ -5,6 +5,6 @@ set -ex
 ubuntu_versions="16.04 18.04"
 for ubuntu_version in ${ubuntu_versions}; do
   image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}:latest"
-  docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} -f ../docker/Dockerfile .
+  docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} -f docker/Dockerfile .
   docker push ${image_name}
 done
