e8a5e60235d2ed7d4dbb9d2429cdb546260e79c7
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-05-15 10:03:54 -0500

    updating install script to new path

--- CI/travis/install.sh
+++ CI/travis/install.sh
@@ -8,11 +8,11 @@ cd ${dagmc_build_dir}/DAGMC
 
 # Only build MOAB master and develop; v5.1.0 is already in the docker image
 if [ "${MOAB_VERSION}" == "master" ] || [ "${MOAB_VERSION}" == "develop" ]; then
-  CI/build_moab.sh
+  CI/docker/build_moab.sh
 fi
 
 # Build DAGMC (shared executables)
-build_static_exe=OFF CI/travis.build_dagmc.sh
+build_static_exe=OFF CI/travis/build_dagmc.sh
 
 # Build DAGMC (static executables)
-build_static_exe=ON CI/travis.build_dagmc.sh
+build_static_exe=ON CI/travis/build_dagmc.sh
