ca5dca12c4c1f8fa55f2dbd40b74fca4f78b232e
Paul P.H. Wilson <paul.wilson@wisc.edu>
2020-07-07 06:10:34 -0500

    Continue making both shared and static version of MOAB for now

--- CI/docker/build_moab.sh
+++ CI/docker/build_moab.sh
@@ -28,5 +28,16 @@ cmake ../moab -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
               -DCMAKE_CXX_COMPILER=${CXX}
 make -j${ci_jobs}
 make install
+rm -rf *
+cmake ../moab -DBUILD_SHARED_LIBS=OFF \
+              -DENABLE_HDF5=ON -DHDF5_ROOT=${hdf5_install_dir} \
+              -DENABLE_PYMOAB=ON \
+              -DENABLE_BLASLAPACK=OFF \
+              -DENABLE_FORTRAN=OFF \
+              -DCMAKE_INSTALL_PREFIX=${moab_install_dir} \
+              -DCMAKE_C_COMPILER=${CC} \
+              -DCMAKE_CXX_COMPILER=${CXX}
+make -j${jobs}
+make install
 cd
 rm -rf ${moab_build_dir}
