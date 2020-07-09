6626b9469b4a37c90896f1def02cded39d8e9fa7
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2020-07-09 18:30:27 -0500

    some test

--- CI/update_docker.sh
+++ CI/update_docker.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-ubuntu_versions="16.04 18.04 20.04"
-compilers="gcc clang"
+ubuntu_versions="16.04" # 18.04 20.04"
+compilers="gcc" # clang"
 hdf5_versions="1.10.4"
 moab_versions="5.1.0"
 for ubuntu_version in ${ubuntu_versions}; do
@@ -15,14 +15,14 @@ for ubuntu_version in ${ubuntu_versions}; do
     image_name="svalinn/test_dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-g4"
     docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
                                   --build-arg COMPILER=${compiler} \
-                 -f CI/Dockerfile_1_g4 .
+                 -f CI/Dockerfile_1_g4 . --no-cache
     docker push ${image_name}
     for hdf5 in $hdf5_versions; do
       image_name="svalinn/test_dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-g4-hdf5_${hdf5}"
       docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
                                     --build-arg COMPILER=${compiler} \
                                     --build-arg HDF5=${hdf5} \
-                   -f CI/Dockerfile_2_hdf5 .
+                   -f CI/Dockerfile_2_hdf5 . --no-cache
       docker push ${image_name}
       for moab in $moab_versions; do
         image_name="svalinn/test_dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-g4-hdf5_${hdf5}-moab_${moab}"
@@ -30,7 +30,7 @@ for ubuntu_version in ${ubuntu_versions}; do
                                       --build-arg COMPILER=${compiler} \
                                       --build-arg HDF5=${hdf5} \
                                       --build-arg MOAB=${moab} \
-                     -f CI/Dockerfile_2_hdf5 .
+                     -f CI/Dockerfile_2_hdf5 . --no-cache
         docker push ${image_name}
       done
     done
