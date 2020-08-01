55f303bc05168782c8b2166239706cd2dddc7f1a
Patrick Shriwise <pshriwise@gmail.com>
2020-08-01 11:22:10 -0500

    More explicit change dir for the double down build script.

--- CI/travis/build_double_down.sh
+++ CI/travis/build_double_down.sh
@@ -10,7 +10,7 @@ cd ${double_down_build_dir}
 git clone https://github.com/pshriwise/double-down
 ln -snf double-down src
 
-cd bld
+cd ${double_down_build_dir}/bld
 cmake ../src -DCMAKE_INSTALL_PREFIX=${double_down_install_dir} \
       -DMOAB_DIR=${moab_install_dir} -DEMBREE_DIR=$HOME/EMBREE/
 make -j${jobs}
