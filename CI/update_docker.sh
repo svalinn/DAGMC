758f0eb84471eff6b1013ab60b36b29fe500a15e
Patrick Shriwise <pshriwise@gmail.com>
2020-08-01 10:46:52 -0500

    Adding a dry run flag to the update docker script.

--- CI/update_docker.sh
+++ CI/update_docker.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+PUSH=true
+
+# check for dry-run flag
+for arg in "$@"
+do
+  case $arg in
+      -n|--no-push)
+      PUSH=false
+      shift
+      ;;
+  esac
+done
+
 ubuntu_versions="16.04 18.04"
 compilers="gcc clang"
 hdf5_versions="1.10.4"
@@ -10,20 +23,20 @@ for ubuntu_version in ${ubuntu_versions}; do
   image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}"
   docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
                -f CI/Dockerfile_0_base .
-  docker push ${image_name}
+  if [ "$PUSH" = true ]; then docker push ${image_name}; fi
   for compiler in $compilers; do
     image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-ext"
     docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
                                   --build-arg COMPILER=${compiler} \
-                 -f CI/Dockerfile_1_g4 .
-    docker push ${image_name}
+                 -f CI/Dockerfile_1_external_deps .
+    if [ "$PUSH" = true ]; then docker push ${image_name}; fi
     for hdf5 in $hdf5_versions; do
       image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-ext-hdf5_${hdf5}"
       docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
                                     --build-arg COMPILER=${compiler} \
                                     --build-arg HDF5=${hdf5} \
                    -f CI/Dockerfile_2_hdf5 .
-      docker push ${image_name}
+      if [ "$PUSH" = true ]; then docker push ${image_name}; fi
       for moab in $moab_versions; do
         image_name="svalinn/dagmc-ci-ubuntu-${ubuntu_version}-${compiler}-ext-hdf5_${hdf5}-moab_${moab}"
         docker build -t ${image_name} --build-arg UBUNTU_VERSION=${ubuntu_version} \
@@ -31,7 +44,7 @@ for ubuntu_version in ${ubuntu_versions}; do
                                       --build-arg HDF5=${hdf5} \
                                       --build-arg MOAB=${moab} \
                      -f CI/Dockerfile_3_moab .
-        docker push ${image_name}
+        if [ "$PUSH" = true ]; then docker push ${image_name}; fi
       done
     done
   done
@@ -39,4 +52,4 @@ done
 
 # Building image for houe keeping
 docker build -t svalinn/dagmc-ci-ubuntu-18.04-housekeeping -f CI/Dockerfile_1_housekeeping .
-docker push svalinn/dagmc-ci-ubuntu-18.04-housekeeping
+if [ "$PUSH" = true ]; then docker push svalinn/dagmc-ci-ubuntu-18.04-housekeeping; fi
