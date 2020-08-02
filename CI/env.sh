4493d26a957fb1fd972626e6169ff8b49facea6d
Patrick Shriwise <pshriwise@gmail.com>
2020-08-01 20:35:45 -0500

    Resetting environment file.

--- CI/env.sh
+++ CI/env.sh
@@ -4,29 +4,8 @@ export jobs=`grep -c processor /proc/cpuinfo`
 
 export CTEST_OUTPUT_ON_FAILURE=1
 
-if [ "${HDF5_VERSION}" == "system" ]; then
-  export hdf5_build_dir=
-  export hdf5_install_dir=/usr/lib/x86_64-linux-gnu/hdf5/serial
-else
-  export hdf5_build_dir=${build_dir}/hdf5-${HDF5_VERSION}
-  export hdf5_install_dir=${install_dir}/hdf5-${HDF5_VERSION}
-fi
-
-export hdf5_1_10_4_shasum=8f60dc4dd6ab5fcd23c750d1dc5bca3d0453bdce5c8cdaf0a4a61a9d1122adb2
-
-
-export geant4_build_dir=${build_dir}/geant4-${geant4_version}
-export geant4_install_dir=${install_dir}/geant4-${geant4_version}
-
-export moab_build_dir=${build_dir}/moab-${MOAB_VERSION}-hdf5-${HDF5_VERSION}
-export moab_install_dir=${install_dir}/moab-${MOAB_VERSION}-hdf5-${HDF5_VERSION}
-
-export double_down_build_dir=${build_dir}/double-down/
-export double_down_install_dir=${install_dir}/double-down/
-
-export dagmc_build_dir=${build_dir}/DAGMC-moab-${MOAB_VERSION}-hdf5-${HDF5_VERSION}
-export dagmc_install_dir=${install_dir}/DAGMC-moab-${MOAB_VERSION}-hdf5-${HDF5_VERSION}
-
+export dagmc_build_dir=${build_dir}/DAGMC
+export dagmc_install_dir=${install_dir}/DAGMC
 export dagmc_build_dir_shared=${dagmc_build_dir}-shared
 export dagmc_install_dir_shared=${dagmc_install_dir}-shared
 
