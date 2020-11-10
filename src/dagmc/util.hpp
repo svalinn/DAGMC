882d57e445c0b7d51932cb012597824ceae8c34b
Patrick Shriwise <pshriwise@gmail.com>
2020-11-10 15:04:17 -0600

    Adding a utility header and function for transforming a string to lowercase characters in place.

---
+++ src/dagmc/util.hpp
@@ -0,0 +1,14 @@
+
+
+// include guard
+#ifndef _DAGMC_UTIL
+#define _DAGMC_UTIL
+
+#include <string>
+
+inline void lowercase_str(std::string& input) {
+    std::transform(input.begin(), input.end(), input.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+}
+
+#endif
