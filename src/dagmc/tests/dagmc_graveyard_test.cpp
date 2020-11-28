99fc6fbc29a6e977d276ec30a5233ca8cf019fcc
Patrick Shriwise <pshriwise@gmail.com>
2020-11-27 20:39:13 -0600

    Adding checks that the number/type  of entities and entity sets remain the same before and after graveyard removal.

--- src/dagmc/tests/dagmc_graveyard_test.cpp
+++ src/dagmc/tests/dagmc_graveyard_test.cpp
@@ -31,15 +31,31 @@ class GraveyardTest : public::testing::Test {
 TEST_F(DagmcGraveyardTest, dagmc_load_file) {
   DAG = new DagMC();
   ErrorCode rval = DAG->load_file(input_file); // open the Dag file
-  EXPECT_EQ(rval, MB_SUCCESS);
+  EXPECT_EQ(MB_SUCCESS, rval);
 
   rval = DAG->init_OBBTree();
   EXPECT_EQ(MB_SUCCESS, rval);
 
+  Range starting_vertices;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBVERTEX, starting_vertices);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
+  Range starting_triangles;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBTRI, starting_triangles);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
+  Range starting_sets;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBENTITYSET, starting_sets);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
   EXPECT_FALSE(DAG->has_graveyard());
+
+  int n_vols = DAG->num_entities(3);
 
   rval = DAG->create_graveyard();
   EXPECT_EQ(MB_SUCCESS, rval);
+
+  EXPECT_EQ(n_vols + 1, DAG->num_entities(3));
 
   EXPECT_TRUE(DAG->has_graveyard());
 
@@ -57,4 +73,27 @@ TEST_F(DagmcGraveyardTest, dagmc_load_file) {
   EXPECT_EQ(MB_SUCCESS, rval);
 
   EXPECT_FALSE(DAG->has_graveyard());
+
+  // checks to make sure we didn't accumulate any new data on the mesh
+  Range ending_vertices;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBVERTEX, ending_vertices);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
+  Range ending_triangles;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBTRI, ending_triangles);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
+  Range ending_sets;
+  rval = DAG->moab_instance()->get_entities_by_type(0, MBENTITYSET, ending_sets);
+  EXPECT_EQ(MB_SUCCESS, rval);
+
+  EXPECT_EQ(starting_vertices.size(), ending_vertices.size());
+  EXPECT_EQ(0, subtract(starting_vertices, ending_vertices).size());
+
+  EXPECT_EQ(starting_triangles.size(), ending_triangles.size());
+  EXPECT_EQ(0, subtract(starting_triangles, ending_triangles).size());
+
+  EXPECT_EQ(starting_sets.size(), ending_sets.size());
+  EXPECT_EQ(0, subtract(starting_sets, ending_sets).size());
+
 }
