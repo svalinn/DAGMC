37200fb46353c90706cb231e360671e9cbe4c655
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-03-19 13:01:57 -0500

    better ?

--- src/pyne/amalgamate_pyne.sh
+++ src/pyne/amalgamate_pyne.sh
@@ -14,12 +14,11 @@ python amalgamate.py -f license.txt src/utils.* src/extra_types.h src/h5wrap.h \
     src/source_sampling.*
 cp pyne.cpp pyne.h $2
 githash=`git rev-parse HEAD`
-cd ..
-
+cd $2
 python remove_unsupported.py
 mv -fv pyne.cpp.new pyne.cpp
 
 # Update source.F90
-cp -v pyne/share/source.F90       ../mcnp/mcnp5/pyne_source/source.F90
-cp -v pyne/share/source_mcnp6.F90 ../mcnp/mcnp6/pyne_source/source.F90
+cp -v $1/pyne/share/source.F90       $1/../mcnp/mcnp5/pyne_source/source.F90
+cp -v $1/pyne/share/source_mcnp6.F90 $1/../mcnp/mcnp6/pyne_source/source.F90
 
