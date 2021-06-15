02f82e68fcbb691e36fbdd2a13ad3c9a2403e7aa
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-06-15 10:29:47 -0500

    targeting 9c96d17 MOAB commit instead of version 5.1

--- CI/docker/build_moab.sh
+++ CI/docker/build_moab.sh
@@ -7,7 +7,8 @@ source ${docker_env}
 if [ ${MOAB_VERSION} == "master" ] || [ ${MOAB_VERSION} == "develop" ]; then
   branch=${MOAB_VERSION}
 else
-  branch=Version${MOAB_VERSION}
+#  branch=Version${MOAB_VERSION}
+  commit=${MOAB_VERSION}
 fi
 
 rm -rf ${moab_build_dir}/bld ${moab_install_dir}
