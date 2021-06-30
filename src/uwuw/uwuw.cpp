426b5315a76b4fa1d006be619931f6602c9c45f6
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-06-30 12:09:37 -0500

    cleaning adding consistency

--- src/uwuw/uwuw.cpp
+++ src/uwuw/uwuw.cpp
@@ -1,8 +1,10 @@
 #include "uwuw.hpp"
 
 #ifndef _WIN32
-#include <unistd.h>
+  #include <unistd.h>
+  #include <filesystem>
 #endif
+
 #include <iostream>
 
 // Empty Constructor
@@ -68,7 +70,7 @@ std::string UWUW::get_full_filepath(std::string filename) {
 #ifndef _WIN32
   const char* full_filepath = realpath(filename.c_str(), NULL);
 #else
-  const char* full_filepath = filename.c_str();
+  const char* full_filepath = std::filesystem::canonical(filename.c_str()).string().c_str();
 #endif
   return std::string(full_filepath);
 }
