a9d681dee8f09ac43bb37f09386c653156541ff6
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2021-07-23 13:46:43 -0500

    resetting remove_unsoported and amalgamate_pyne scripts

--- src/pyne/amalgamate_pyne.sh
+++ src/pyne/amalgamate_pyne.sh
@@ -16,9 +16,8 @@ python3 amalgamate.py -f license.txt src/utils.* src/extra_types.h src/h5wrap.h 
     src/json-forwards.h src/json.h src/jsoncpp.cpp src/jsoncustomwriter.* \
     src/material.* src/material_library.* src/tally.* src/atomic_data.* src/measure.* \
     src/source_sampling.*
-
 cp pyne.cpp pyne.h $2
- 
+
 githash=`git rev-parse HEAD`
 cd $2
 python3 $1/remove_unsupported.py
