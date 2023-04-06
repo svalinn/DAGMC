67100ba1f27cf70ec5a513e4112e70a677d4a779
Paul P.H. Wilson <paul.wilson@wisc.edu>
2023-04-06 09:19:57 -0500

    whitespace for clang-format

--- src/dagmc/DagMC.hpp
+++ src/dagmc/DagMC.hpp
@@ -79,9 +79,9 @@ class DagMC {
   // Deprecated Constructor
   [
       [deprecated("Replaced by DagMC(std::shared_ptr<Interface> mb_impl, ... "
-                  ")")]] DagMC(Interface* mb_impl, 
+                  ")")]] DagMC(Interface* mb_impl,
                                double overlap_tolerance = 0.,
-                               double numerical_precision = .001, 
+                               double numerical_precision = .001,
                                int verbosity = 1);
   // Destructor
   ~DagMC();
