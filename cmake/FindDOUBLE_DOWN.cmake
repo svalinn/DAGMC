241b8d4d23fb58a9bc9849752687b0e3539476df
Ahnaf Tahmid Chowdhury <ahnaf-tahmid@outlook.com>
2024-02-22 22:10:59 +0600

    dd_VERSION

--- cmake/FindDOUBLE_DOWN.cmake
+++ cmake/FindDOUBLE_DOWN.cmake
@@ -6,6 +6,7 @@
 #  dd_INCLUDE_DIRS - include directories for installed dd headers
 #  dd_LIBRARY_DIRS - location of installed dd libraries
 #  dd_LIBRARIES    - set of libraries installed with dd, use to link applications against dd
+#  dd_VERSION      - version of installed dd
 
 find_path(dd_CMAKE_CONFIG NAMES ddConfig.cmake
           HINTS ${dd_ROOT} $ENV{dd_ROOT}
@@ -14,6 +15,12 @@ find_path(dd_CMAKE_CONFIG NAMES ddConfig.cmake
           PATH_SUFFIXES lib Lib cmake lib/cmake/ lib/cmake/dd
           NO_DEFAULT_PATH)
 
-message(STATUS "Found dd in ${dd_CMAKE_CONFIG}")
-
-include(${dd_CMAKE_CONFIG}/ddConfig.cmake)
+if (dd_CMAKE_CONFIG)
+    message(STATUS "Found dd in ${dd_CMAKE_CONFIG}")
+    include(${dd_CMAKE_CONFIG}/ddConfig.cmake)
+    include(${dd_CMAKE_CONFIG}/ddConfigVersion.cmake)
+    set(dd_VERSION ${PACKAGE_VERSION})
+    message(STATUS "Found dd version ${dd_VERSION}")
+else()
+    message(FATAL_ERROR "Could not find dd")
+endif()
