35bd304ec77bb2f1720fbca4e59db11e620938d0
Andrew Davis <andrew.davis@ukaea.uk>
2025-05-12 13:02:29 +0100

    Long lived particles might need culling

--- src/geant4/app/src/DagsolidSteppingAction.cc
+++ src/geant4/app/src/DagsolidSteppingAction.cc
@@ -8,7 +8,7 @@ TimeCutSteppingAction::~TimeCutSteppingAction() {
 
 void TimeCutSteppingAction::UserSteppingAction(const G4Step* step) {
     G4Track* track = step->GetTrack();
-    if (track->GetGlobalTime() >= 20.0 * ms) {
+    if (track->GetGlobalTime() >= 1.0 * us) {
         track->SetTrackStatus(fStopAndKill);
         G4cout << "Track killed at time: " << track->GetGlobalTime() / ms << " ms";
         G4cout << " due to time cut" << G4endl;
