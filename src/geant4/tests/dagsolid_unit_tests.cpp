56399edaffebcff82e8f7d9f9d36165a8ab251f2
Andrew Davis <andrew.davis@ukaea.uk>
2025-05-19 15:45:34 +0100

    Explicit namespace for MOAB

--- src/geant4/tests/dagsolid_unit_tests.cpp
+++ src/geant4/tests/dagsolid_unit_tests.cpp
@@ -11,7 +11,7 @@
 class DagSolidTest : public ::testing::Test {
  protected:
   virtual void SetUp() {
-    DagMC* dagmc = new moab::DagMC();  // create dag instance
+    moab::DagMC* dagmc = new moab::DagMC();  // create dag instance
 
     // dag_volumes
     const char* h5mfilename = "test_geom.h5m";
