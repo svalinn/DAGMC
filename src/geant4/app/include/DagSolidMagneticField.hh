1be9c525f7adb56805582736938eb8adb1b7fe22
Andrew Davis <andrew.davis@ukaea.uk>
2026-07-21 11:28:40 +0100

    Missing include guard define

--- src/geant4/app/include/DagSolidMagneticField.hh
+++ src/geant4/app/include/DagSolidMagneticField.hh
@@ -1,5 +1,5 @@
 #ifndef DAG_SOLID_MAGNETIC_FIELD_HH
-#define DAG_SOLID_MAGNETIC_FIELD_HH
+#define DAG_SOLID_MAGNETIC_FIELD_HH 1
 
 #include "G4MagneticField.hh"
 #include "G4SystemOfUnits.hh"
@@ -88,7 +88,7 @@ class MagneticField : public G4MagneticField {
   ~MagneticField() override;
 
   // main lookup function that Geant4 will call
-  void GetFieldValue(const G4double Point[4],
+  void GetFieldValue(const src/ExN01DetectorConstruction.ccG4double Point[4],
                     double *field) const override;                     
 
   // load the file containing white space delimited
