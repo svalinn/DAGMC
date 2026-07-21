cb64b9994ae686492c271194505d3141f203dd71
Andrew Davis <andrew.davis@ukaea.uk>
2026-07-21 12:30:13 +0100

    Add a unit test for wire field

--- src/geant4/app/test/dagsolid_magfield_unit_tests.cpp
+++ src/geant4/app/test/dagsolid_magfield_unit_tests.cpp
@@ -26,83 +26,35 @@ class DagSolidMagneticFieldTest : public ::testing::Test {
   MagneticField* magneticField;
 };
 
+class DagSolidWireMagneticFieldTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    // note wire radius is 10mm, mu is 1, current is 1e6 A
+    magneticField = new WireMagneticField(10,1,1e6);
+  }
+
+ protected:
+  MagneticField* magneticField;
+};
+
 /*
  * Empty common setup function
  */
-TEST_F(DagSolidMagneticFieldTest, SetUp) {}
-
-// Interpolation test
-TEST_F(DagSolidMagneticFieldTest, interpolation_test) {
-  const std::array<double,2> x_stencil = {0.0, 1000};
-  const std::array<double,2> y_stencil = {-1000, 1000};
-  const std::array<double,4> f_values = {1.0, 1.0, 1.0, 1.0};
-
-  EXPECT_EQ(1.0, BilinearInterpolation(x_stencil, y_stencil, f_values, 0., 0.));
-  EXPECT_EQ(1.0, BilinearInterpolation(x_stencil, y_stencil, f_values, 0., 1000.));
-  EXPECT_EQ(1.0, BilinearInterpolation(x_stencil, y_stencil, f_values, 1000., 1000.));
-  EXPECT_EQ(1.0, BilinearInterpolation(x_stencil, y_stencil, f_values, 1000., -1000.));
-
-  const std::array<double,2> x_stencil2 = {0.0, 1000};
-  const std::array<double,2> y_stencil2 = {-1000, 1000};
-  const std::array<double,4> f_values2 = {1.0, 2.0, 3.0, 4.0};
-
-  // check the 4 corners
-  EXPECT_EQ(1.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 0., -1000.));
-  EXPECT_EQ(2.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 0., 1000.));
-  EXPECT_EQ(3.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 1000., -1000.));
-  EXPECT_EQ(4.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 1000., 1000.));
-  // check the middle
-  EXPECT_EQ(2.5, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 500., 0.));
-  // check middle left  
-  EXPECT_EQ(1.5, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 0., 0.));
-  // check middle right
-  EXPECT_EQ(3.5, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 1000., 0.));
-  // check middle top
-  EXPECT_EQ(3.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 500., 1000.));
-  // check middle bottom
-  EXPECT_EQ(2.0, BilinearInterpolation(x_stencil2, y_stencil2, f_values2, 500., -1000.));
-
-  return;
-}
-
-// 
+TEST_F(DagSolidMagneticWireFieldTest, SetUp) {}
 
 //
 TEST_F(DagSolidMagneticFieldTest, sample_test) {
-  G4double point[4] = {500.0, 0.0, 0.0, 0.0};  // mm
+  G4double point[4] = {5.0, 0.0, 0.0, 0.0};  // mm
   G4double field[3];
   magneticField->GetFieldValue(point,field);
 
+  G4double b = 1*1e6/2*CLHEP::pi*5.0;
   // expect field x to be 1.0
-  EXPECT_EQ(1.0, field[0]);
-  EXPECT_EQ(0.0, field[1]);
-  EXPECT_EQ(0.0, field[2]);
-
-  point[0] = 0.0;
-  magneticField->GetFieldValue(point,field);
-  // expect field x to be 1.0
-  EXPECT_EQ(1.0, field[0]);
-  EXPECT_EQ(0.0, field[1]);
-  EXPECT_EQ(0.0, field[2]);
-
-  point[0] = 1000.0;
-  point[1] = 0.0;
-  point[2] = 0.0;
-
-  magneticField->GetFieldValue(point,field);
-  // expect field x to be 1.0
-  EXPECT_EQ(1.0, field[0]);
-  EXPECT_EQ(0.0, field[1]);
-  EXPECT_EQ(0.0, field[2]);
-
-  point[0] = 1100.0;
-  point[1] = 0.0;
-  point[2] = 0.0;
-
-  magneticField->GetFieldValue(point,field);
-  // expect field x to be 0.0
+  G4double field_mag = std::sqrt(field[0]*field[0] + field[1]*field[1] + field[2]*field[2]);
+  EXPECT_NEAR(b, field_mag, 1e-6);
   EXPECT_EQ(0.0, field[0]);
-  EXPECT_EQ(0.0, field[1]);
+  EXPECT_NEAR(b, field[1],1e-6);
   EXPECT_EQ(0.0, field[2]);
+
   return;
 }
