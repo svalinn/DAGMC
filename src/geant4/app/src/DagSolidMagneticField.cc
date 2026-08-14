16c3834ed35b24a30059a63792d82c6f53be5a3f
Andrew Davis <andrew.davis@ukaea.uk>
2026-08-14 10:00:27 +0100

    More debug info

--- src/geant4/app/src/DagSolidMagneticField.cc
+++ src/geant4/app/src/DagSolidMagneticField.cc
@@ -63,8 +63,10 @@ void WireMagneticField::GetFieldValue(const G4double Point[4],
   G4cout << "B: " << B << G4endl;
   G4cout << "i: " << i << " j: " << j << G4endl;
   G4cout << "r2: " << r2 << " sqrt(r2): " << std::sqrt(r2) << G4endl;
-  G4cout << Point[0] << " " << Point[1] << " " << Point[2] << G4endl;
-  G4cout << field[0] << " " << field[1] << " " << field[2] << G4endl;
+  G4cout << "Field x: " << B*i/std::sqrt(r2) << G4endl;
+  G4cout << "Field y: " << B*j/std::sqrt(r2) << G4endl;
+  G4cout << "Point: " << Point[0] << " " << Point[1] << " " << Point[2] << G4endl;
+  G4cout << "Field: " << field[0] << " " << field[1] << " " << field[2] << G4endl;
   G4cout << "Debug " << G4endl;
   return;
 }
